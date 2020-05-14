eff3382c93e53dfeb73ba4af4795e36fa95d421b
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-05-14 15:48:49 -0700

    Added http module

--- suppliers/src/UI/embc-supplier/src/app/app.component.ts
+++ suppliers/src/UI/embc-supplier/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SupplierHttpService } from './service/supplierHttp.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'embc-supplier';
+
+  constructor(private supplierHttp: SupplierHttpService) {}
+
+  ngOnInit() {
+    this.setUpData();
+  }
+
+  setUpData() {
+    this.supplierHttp.getListOfCities();
+  }
 }
---
+++ suppliers/src/UI/embc-supplier/src/app/service/supplierHttp.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SupplierHttpService {
+
+     get headers(): HttpHeaders {
+        return new HttpHeaders({ 'Content-Type': 'application/json' });
+      }
+
+    constructor(private http: HttpClient) {}
+
+    getListOfCities() {
+        this.http.get(`http://localhost:8080/api/Lists/communities`, {headers: this.headers}).subscribe((res) => {
+            console.log(res);
+        })
+    }
+
+}
