825fc02a83dc37021e8f5e14adf4f95b49602b99
Yossi Tamari <Yossi.Tamari@quartech.com>
2020-05-20 14:51:47 -0700

    Changed reference number generator to include date and prefix

---
+++ suppliers/src/API/EMBC.Tests.Unit.Suppliers.API/SubmissionModule/ReferenceNumberGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using EMBC.Suppliers.API.SubmissionModule.Models;
+using Xunit;
+
+namespace EMBC.Tests.Unit.Suppliers.API.SubmissionModule
+{
+    public class ReferenceNumberGeneratorTests
+    {
+        [Fact]
+        public void Generate_TemplatedReferenceNumber()
+        {
+            var referenceNumber = ReferenceNumberGenerator.CreateNew();
+            Assert.StartsWith($"SUP-{DateTime.Today:yyyyMMdd}", referenceNumber);
+        }
+
+        [Fact]
+        public void Generate_MultipleTimes_UniqueReferenceNumber()
+        {
+            var numberOfIterations = 1000;
+            var referenceNumbers = new string[numberOfIterations];
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                referenceNumbers[i] = ReferenceNumberGenerator.CreateNew();
+            }
+            Assert.Equal(numberOfIterations, referenceNumbers.Distinct().Count());
+        }
+    }
+}
