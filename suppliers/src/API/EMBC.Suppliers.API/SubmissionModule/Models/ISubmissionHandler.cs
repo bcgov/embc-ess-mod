152c0b3f91c6dc4db949d3bc244a99808c691a74
Yossi Tamari <Yossi.Tamari@quartech.com>
2020-05-25 17:14:57 -0700

    Added non prod GET for submissions

--- suppliers/src/API/EMBC.Suppliers.API/SubmissionModule/Models/ISubmissionHandler.cs
+++ suppliers/src/API/EMBC.Suppliers.API/SubmissionModule/Models/ISubmissionHandler.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
+using EMBC.Suppliers.API.SubmissionModule.ViewModels;
 
 namespace EMBC.Suppliers.API.SubmissionModule.Models
 {
     public interface ISubmissionHandler
     {
         Task<string> Handle(PersistSupplierSubmissionCommand cmd);
+
+        Task<Submission> Handle(GetSupplierSubmissionCommand cmd);
     }
 }
