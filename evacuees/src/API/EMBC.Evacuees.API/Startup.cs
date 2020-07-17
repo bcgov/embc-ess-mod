4ed697254ae6c3a6f9360cd6e791d361986d3ba7
Yossi Tamari <Yossi.Tamari@quartech.com>
2020-07-17 13:25:44 -0700

    infrastructure code for evacuee portal

--- evacuees/src/API/EMBC.Evacuees.API/Startup.cs
+++ evacuees/src/API/EMBC.Evacuees.API/Startup.cs
@@ -1,39 +1,111 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2020 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+
+using System.IO;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSwag.AspNetCore;
+using Serilog;
 
 namespace EMBC.Evacuees.API
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IHostEnvironment env;
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration, IHostEnvironment env)
         {
+            this.configuration = configuration;
+            this.env = env;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new HttpResponseExceptionFilter());
+            });
+            var dpBuilder = services.AddDataProtection();
+            var keyRingPath = configuration.GetValue("KEY_RING_PATH", string.Empty);
+            if (!string.IsNullOrWhiteSpace(keyRingPath))
+            {
+                dpBuilder.PersistKeysToFileSystem(new DirectoryInfo(keyRingPath));
+            }
+
+            services.AddOpenApiDocument();
+            services.Configure<OpenApiDocumentMiddlewareSettings>(options =>
+            {
+                options.Path = "/api/swagger/{documentName}/swagger.json";
+            });
+            services.Configure<SwaggerUi3Settings>(options =>
+            {
+                options.Path = "/api/swagger";
+                options.DocumentPath = "/api/swagger/{documentName}/swagger.json";
+            });
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.All;
+                var knownNetworks = configuration.GetValue("KNOWN_NETWORKS", "::ffff:172.51.0.0/16").Split(';');
+                foreach (var knownNetwork in knownNetworks)
+                {
+                    options.KnownNetworks.Add(ParseNetworkFromString(knownNetwork));
+                }
+            });
+            services.AddDistributedMemoryCache();
+        }
+
+        private IPNetwork ParseNetworkFromString(string network)
+        {
+            var networkParts = network.Trim().Split('/');
+            var prefix = IPAddress.Parse(networkParts[0]);
+            var length = int.Parse(networkParts[1]);
+            return new IPNetwork(prefix, length);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/error-local-development");
             }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseSerilogRequestLogging();
+            app.UseForwardedHeaders();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
