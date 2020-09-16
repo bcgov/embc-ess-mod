ffafd75d860a3e6735f2e73d7e2682c4e27338fb
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-09-16 11:41:30 -0700

    fixed lint issues-3

--- evacuees/src/UI/embc-evacuee/src/app/core/components/needs-assessment-forms/pets/pets.component.spec.ts
+++ evacuees/src/UI/embc-evacuee/src/app/core/components/needs-assessment-forms/pets/pets.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import  PetsComponent  from './pets.component';
+import PetsComponent from './pets.component';
 
 describe('PetsComponent', () => {
   let component: PetsComponent;
