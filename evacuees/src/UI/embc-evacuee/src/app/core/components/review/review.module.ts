3666f6d96f9d85c8a9b20bde76cf0b1090353f26
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-10-27 12:49:26 -0700

    EMBCESSMOD-728: Review Page UI

--- evacuees/src/UI/embc-evacuee/src/app/core/components/review/review.module.ts
+++ evacuees/src/UI/embc-evacuee/src/app/core/components/review/review.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ReviewComponent } from './review.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CustomPipeModule } from '../../pipe/customPipe.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    CustomPipeModule
   ],
   exports: [
     ReviewComponent
