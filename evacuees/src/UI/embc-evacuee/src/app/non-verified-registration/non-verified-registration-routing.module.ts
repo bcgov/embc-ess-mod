0ab1951fa535a2dde27b232b62f44cf075c2a667
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-11-02 10:07:17 -0800

    code refractor

--- evacuees/src/UI/embc-evacuee/src/app/non-verified-registration/non-verified-registration-routing.module.ts
+++ evacuees/src/UI/embc-evacuee/src/app/non-verified-registration/non-verified-registration-routing.module.ts
@@ -13,19 +13,19 @@ const routes: Routes = [
       },
       {
         path: 'collection-notice',
-        loadChildren: () => import('../sharedRoutes/collection-notice/collection-notice.module').then(m => m.CollectionNoticeModule)
+        loadChildren: () => import('../sharedModules/collection-notice/collection-notice.module').then(m => m.CollectionNoticeModule)
       },
       {
         path: 'restriction',
-        loadChildren: () => import('../sharedRoutes/restriction/restriction.module').then(m => m.RestrictionModule)
+        loadChildren: () => import('../sharedModules/restriction/restriction.module').then(m => m.RestrictionModule)
       },
       {
         path: 'create-profile',
-        loadChildren: () => import('../sharedRoutes/profile/profile.module').then(m => m.ProfileModule)
+        loadChildren: () => import('../sharedModules/profile/profile.module').then(m => m.ProfileModule)
       }, /// :stepPos
       {
         path: 'needs-assessment',
-        loadChildren: () => import('../sharedRoutes/needs-assessment/needs-assessment.module').then(m => m.NeedsAssessmentModule)
+        loadChildren: () => import('../sharedModules/needs-assessment/needs-assessment.module').then(m => m.NeedsAssessmentModule)
       },
       {
         path: 'edit/:type',
@@ -33,7 +33,7 @@ const routes: Routes = [
       },
       {
         path: 'fileSubmission',
-        loadChildren: () => import('../sharedRoutes/file-submission/file-submission.module').then(m => m.FileSubmissionModule)
+        loadChildren: () => import('../sharedModules/file-submission/file-submission.module').then(m => m.FileSubmissionModule)
       }
     ]
   }
