a5dfe57bd9b3318ec943d60a77a70169b0a9d430
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-12-14 16:27:25 -0800

    EMBCESSMOD-958

---
+++ responders/src/UI/embc-responder/src/app/shared/components/task-search/task-search.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-search',
+  templateUrl: './task-search.component.html',
+  styleUrls: ['./task-search.component.scss']
+})
+export class TaskSearchComponent implements OnInit {
+
+  showSearch = true;
+  isActive = false;
+  isExpired =false;
+  isIncorrect = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+  submitTask(): void {
+    this.showSearch = !this.showSearch;
+    //this.isActive = !this.isActive;
+    this.isExpired = !this.isExpired;
+  }
+
+  searchTask(): void {
+    this.showSearch = !this.showSearch;
+    //this.isActive = !this.isActive;
+    this.isExpired = !this.isExpired;
+  }
+
+  signInTask(): void {
+
+  }
+
+}
