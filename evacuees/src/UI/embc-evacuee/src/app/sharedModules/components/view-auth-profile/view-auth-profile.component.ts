98d8b85a6e5979ec12a2576d13e4d595bac15b9e
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2020-12-14 18:45:31 -0800

    Registrant Portal Tabs implementations

--- evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.component.ts
+++ evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormCreationService } from 'src/app/core/services/formCreation.service';
+
+export interface evacuationCard {
+  date: string;
+  code: number;
+  support: string;
+  status: string;
+}
+
+const ACTIVE_DATA: evacuationCard[] = [
+  {date: "20-Feb-2020", code: 333333, support: "No", status: 'Active'},
+];
+
+const INACTIVE_DATA: evacuationCard[] = [
+  {date: "20-Feb-2020", code: 123456, support: "No", status: 'Inactive'},
+  {date: "20-Feb-2020", code: 123456, support: "No", status: 'Inactive'},
+];
 
 @Component({
   selector: 'app-view-auth-profile',
   templateUrl: './view-auth-profile.component.html',
   styleUrls: ['./view-auth-profile.component.scss']
 })
+
+
 export class ViewAuthProfileComponent implements OnInit {
 
   type = 'profile';
   currentFlow: string;
   parentPageName = 'view-profile';
+  dataSourceActive = ACTIVE_DATA;
+  dataSourceInactive = INACTIVE_DATA;
+  showHeader = true;
+
+  @ViewChild('activeFileDetails') activeFileDetails: TemplateRef<any>;
+
 
   constructor(private route: ActivatedRoute, public formCreationService: FormCreationService, private router: Router) { }
 
@@ -21,6 +45,14 @@ export class ViewAuthProfileComponent implements OnInit {
 
   startAssessment(): void {
     this.router.navigate(['/verified-registration/confirm-restriction']);
+  }
+
+  setHeaders(event: boolean): void {
+    this.showHeader = event;
+  }
+
+  actionTest(){
+    console.log("HOLA");
   }
 
 }
--- evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.module.ts
+++ evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.module.ts
@@ -4,19 +4,29 @@ import { CommonModule } from '@angular/common';
 import { ViewAuthProfileRoutingModule } from './view-auth-profile-routing.module';
 import { ViewAuthProfileComponent } from './view-auth-profile.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ReviewModule } from '../../components/review/review.module';
+import { EvacuationCardComponent } from '../../components/evacuation-card/evacuation-card.component';
+
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
-  declarations: [ViewAuthProfileComponent],
+  declarations: [
+    ViewAuthProfileComponent,
+    EvacuationCardComponent
+  ],
   imports: [
     CommonModule,
     ViewAuthProfileRoutingModule,
     MatCardModule,
+    MatTableModule,
     MatButtonModule,
     MatTabsModule,
-    ReviewModule
+    ReviewModule,
+    MatIconModule
   ]
 })
 export class ViewAuthProfileModule { }
