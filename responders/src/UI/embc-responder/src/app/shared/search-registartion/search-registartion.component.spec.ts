007147660a1a549798bb8d2b09e9ea0dbd2a5bc0
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-12-14 16:25:56 -0800

    EMBCESSMOD-948

---
+++ responders/src/UI/embc-responder/src/app/shared/search-registartion/search-registartion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchRegistartionComponent } from './search-registartion.component';
+
+describe('SearchRegistartionComponent', () => {
+  let component: SearchRegistartionComponent;
+  let fixture: ComponentFixture<SearchRegistartionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchRegistartionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchRegistartionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
