6b9c5f48eb4cbef827118752a19dbbffc415bc89
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2020-12-15 16:41:59 -0800

    Registrant Profile
    Adding new Evacuation File Component details and navigation behaviour through Registrant Portal

--- evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.component.ts
+++ evacuees/src/UI/embc-evacuee/src/app/sharedModules/components/view-auth-profile/view-auth-profile.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormCreationService } from 'src/app/core/services/formCreation.service';
+import { EvacuationCardComponent } from '../evacuation-card/evacuation-card.component'
 
 export interface evacuationCard {
   date: string;
@@ -11,6 +12,8 @@ export interface evacuationCard {
 
 const ACTIVE_DATA: evacuationCard[] = [
   {date: "20-Feb-2020", code: 333333, support: "No", status: 'Active'},
+  {date: "20-Feb-2020", code: 444444, support: "No", status: 'Active'},
+  {date: "20-Feb-2020", code: 555555, support: "No", status: 'Active'},
 ];
 
 const INACTIVE_DATA: evacuationCard[] = [
@@ -27,17 +30,20 @@ const INACTIVE_DATA: evacuationCard[] = [
 
 export class ViewAuthProfileComponent implements OnInit {
 
+  //@ViewChild(EvacuationCardComponent) cardComponent;
+
   type = 'profile';
   currentFlow: string;
   parentPageName = 'view-profile';
   dataSourceActive = ACTIVE_DATA;
   dataSourceInactive = INACTIVE_DATA;
-  showHeader = true;
-
-  @ViewChild('activeFileDetails') activeFileDetails: TemplateRef<any>;
+  showActiveList = true;
+  showInactiveList = true;
+  currentChild: EvacuationCardComponent;
 
 
   constructor(private route: ActivatedRoute, public formCreationService: FormCreationService, private router: Router) { }
+  
 
   ngOnInit(): void {
     this.currentFlow = this.route.snapshot.data.flow;
@@ -47,12 +53,32 @@ export class ViewAuthProfileComponent implements OnInit {
     this.router.navigate(['/verified-registration/confirm-restriction']);
   }
 
-  setHeaders(event: boolean): void {
-    this.showHeader = event;
+
+  setActiveListView(event: boolean): void {
+    this.showActiveList = event;
+    
   }
 
-  actionTest(){
-    console.log("HOLA");
+  setInactiveListView(event: boolean): void {
+    this.showInactiveList = event;
+    
+  }
+
+  setCurrentChild(fileCard: EvacuationCardComponent): void {
+    this.currentChild = fileCard;
+  }
+
+  goBackActive(){
+    this.showActiveList = !this.showActiveList;
+  }
+
+  goBackInactive(){
+    this.showInactiveList = !this.showInactiveList;
+  }
+
+  resetTab($event){
+    this.showActiveList = true;
+    this.showInactiveList = true;
   }
 
 }
---
+++ evacuees/src/UI/embc-evacuee/src/app/sharedModules/evacuation-details/evacuation-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EvacuationDetailsComponent } from './evacuation-details.component';
+
+describe('EvacuationDetailsComponent', () => {
+  let component: EvacuationDetailsComponent;
+  let fixture: ComponentFixture<EvacuationDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EvacuationDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EvacuationDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
