71936dc1b3c566b445a60099ac2c86afab531e74
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-12-21 14:49:32 -0800

    Loader and alert modules

---
+++ evacuees/src/UI/embc-evacuee/src/app/core/components/alert/alert.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Alert } from '../../model/alert';
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+    selector: 'app-alert',
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent implements OnInit{
+
+    type: string;
+    alerts: Alert[] = [];
+
+    constructor(private alertService: AlertService) {}
+
+    /**
+     * Subscribes to the alert subject and set them in the array
+     * if not empty
+     */
+    ngOnInit() {
+        this.alertService.getAlerts().subscribe((alert: Alert) => {
+            if (!alert) {
+                this.alerts = [];
+                return;
+            }
+            this.alerts.push(alert);
+        });
+    }
+
+    /**
+     * Removes the alert from the array
+     * @param alert Alert to be deleted
+     */
+    close(alert: Alert) {
+        this.alerts.splice(this.alerts.indexOf(alert), 1);
+      }
+
+}
