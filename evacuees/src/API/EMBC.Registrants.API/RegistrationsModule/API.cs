c07eab4876091ca3bbeef5e41284f5a8e3aeb683
Rafael Ponce de Leon <Rafael.PonceDeLeon@quartech.com>
2021-01-05 17:33:55 -0800

    EMBCESSMOD-891 Needs Assessment wording update. Save what the Evacuee is able to provide for themselves rather then what they need

--- evacuees/src/API/EMBC.Registrants.API/RegistrationsModule/API.cs
+++ evacuees/src/API/EMBC.Registrants.API/RegistrationsModule/API.cs
@@ -241,11 +241,16 @@ namespace EMBC.Registrants.API.RegistrationsModule
         [Required]
         public InsuranceOption Insurance { get; set; }
 
-        public bool? RequiresFood { get; set; }
-        public bool? RequiresTransportation { get; set; }
-        public bool? RequiresLodging { get; set; }
-        public bool? RequiresClothing { get; set; }
-        public bool? RequiresIncidentals { get; set; }
+        public bool? RequiresFood { get; set; } //To be deleted
+        public bool? RequiresTransportation { get; set; } //To be deleted
+        public bool? RequiresLodging { get; set; } //To be deleted
+        public bool? RequiresClothing { get; set; } //To be deleted
+        public bool? RequiresIncidentals { get; set; } //To be deleted
+        public bool? CanEvacueeProvideFood { get; set; }
+        public bool? CanEvacueeProvideLodging { get; set; }
+        public bool? CanEvacueeProvideClothing { get; set; }
+        public bool? CanEvacueeProvideTransportation { get; set; }
+        public bool? CanEvacueeProvideIncidentals { get; set; }
         public bool HaveSpecialDiet { get; set; }
         public bool HaveMedication { get; set; }
         public IEnumerable<PersonDetails> FamilyMembers { get; set; } = Array.Empty<PersonDetails>();
