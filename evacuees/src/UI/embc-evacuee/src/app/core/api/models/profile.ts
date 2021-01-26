bc1a9e1ad152763240c024a2caa6b19883ad89f3
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-01-25 16:07:35 -0800

    Profile auto-gen api

--- evacuees/src/UI/embc-evacuee/src/app/core/api/models/profile.ts
+++ evacuees/src/UI/embc-evacuee/src/app/core/api/models/profile.ts
@@ -8,8 +8,12 @@ import { PersonDetails } from './person-details';
  * Registrant's profile
  */
 export interface Profile {
+  bceId?: null | string;
   contactDetails: ContactDetails;
+  eraId?: null | string;
   mailingAddress?: null | Address;
   personalDetails: PersonDetails;
   primaryAddress: Address;
+  restrictedAccess?: null | any;
+  secretPhrase?: null | any;
 }
--- evacuees/src/UI/embc-evacuee/src/app/core/api/services/profile.service.ts
+++ evacuees/src/UI/embc-evacuee/src/app/core/api/services/profile.service.ts
@@ -25,7 +25,7 @@ export class ProfileService extends BaseService {
   /**
    * Path part for operation profileGetProfile
    */
-  static readonly ProfileGetProfilePath = '/api/profile';
+  static readonly ProfileGetProfilePath = '/api/profiles/current';
 
   /**
    * This method provides access to the full `HttpResponse`, allowing access to response headers.
@@ -62,6 +62,52 @@ export class ProfileService extends BaseService {
 
     return this.profileGetProfile$Response(params).pipe(
       map((r: StrictHttpResponse<Profile>) => r.body as Profile)
+    );
+  }
+
+  /**
+   * Path part for operation profileUpdate
+   */
+  static readonly ProfileUpdatePath = '/api/profiles/current';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `profileUpdate()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  profileUpdate$Response(params: {
+    body: Profile
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, ProfileService.ProfileUpdatePath, 'post');
+    if (params) {
+      rb.body(params.body, 'application/json');
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `profileUpdate$Response()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  profileUpdate(params: {
+    body: Profile
+  }): Observable<void> {
+
+    return this.profileUpdate$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
     );
   }
 
