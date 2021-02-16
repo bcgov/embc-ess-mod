d2745450fbe10c7723d776bb9b7591ebc0eca093
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-02-16 15:59:01 -0800

    display community list

--- responders/src/UI/embc-responder/src/app/shared/components/data-table/data-table.component.ts
+++ responders/src/UI/embc-responder/src/app/shared/components/data-table/data-table.component.ts
@@ -1,49 +1,34 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { TableColumnModel } from 'src/app/core/models/table-column.model';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+export class DataTableComponent implements AfterViewInit, OnChanges {
+
+  // add optional sorting, filtering and clickable rows
+
+  @Input() displayedColumns: TableColumnModel[];
+  @Input() incomingData = [];
+  dataSource = new MatTableDataSource();
+  columns: string[];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['incomingData']) {
+      this.dataSource = new MatTableDataSource(this.incomingData);
+    }
+    if(changes['displayedColumns']) {
+      this.columns = this.displayedColumns.map(column => column.ref);
+    }
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
