52587822f9b49666f6a3d90f41bf841a4af0842f
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-02-18 14:30:13 -0800

    EMBCESSMOD-1153

--- responders/src/UI/embc-responder/src/app/feature-components/assigned-community/community-list/community-list.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/assigned-community/community-list/community-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TableColumnModel } from 'src/app/core/models/table-column.model';
+import { TableFilterValueModel } from 'src/app/core/models/table-filter-value.model';
+import { TableFilterModel } from 'src/app/core/models/table-filter.model';
 
 @Component({
   selector: 'app-community-list',
@@ -7,6 +9,9 @@ import { TableColumnModel } from 'src/app/core/models/table-column.model';
   styleUrls: ['./community-list.component.scss']
 })
 export class CommunityListComponent implements OnInit {
+
+  filterTerm: TableFilterValueModel;
+  filtersToLoad: TableFilterModel;
 
   constructor() { }
 
@@ -18,12 +23,36 @@ export class CommunityListComponent implements OnInit {
   ];
 
   sampleCommunityData = [
-    {community: 'Community Name', regionalDistrict: 'Regional District Name', type: 'First Nations Community', date: 'mm/dd/yyyy'},
-    {community: 'Community Name', regionalDistrict: 'Regional District Name', type: 'First Nations Community', date: 'mm/dd/yyyy'},
-    {community: 'Community Name', regionalDistrict: 'Regional District Name', type: 'First Nations Community', date: 'mm/dd/yyyy'}
+    {community: 'Community Name', regionalDistrict: 'Cariboo', type: 'First Nations Community', date: 'mm/dd/yyyy'},
+    {community: 'Community Name', regionalDistrict: 'Victoria', type: 'City', date: 'mm/dd/yyyy'},
+    {community: 'Community Name', regionalDistrict: 'Cariboo', type: 'First Nations Community', date: 'mm/dd/yyyy'}
   ];
 
+  regionalDistrictList: string[] = ['Cariboo', 'Victoria', 'Comox Valley'];
+  typesList: string[] = ['First Nations Community', 'City'];
+
   ngOnInit(): void {
+
+    this.filtersToLoad = {
+      loadDropdownFilters: [{
+        type: 'regionalDistrict',
+        label: 'All Regional Districts',
+        values: this.regionalDistrictList
+      },
+      {
+        type: 'type',
+        label: 'All Types',
+        values: this.typesList
+      }],
+      loadInputFilter: {
+        type: 'Search by city, town, village or community',
+        label: 'Search by city, town, village or community'
+      }
+    }
+  }
+
+  filter(event: TableFilterValueModel) {
+    this.filterTerm = event;
   }
 
 }
