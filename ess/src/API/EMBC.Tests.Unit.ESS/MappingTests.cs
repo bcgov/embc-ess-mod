8636c03b68ae0722c04791c35b754fb25be555ff
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-03-01 14:11:06 -0800

    ess backend unit tests

---
+++ ess/src/API/EMBC.Tests.Unit.ESS/MappingTests.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using EMBC.ESS;
+using Xunit;
+
+namespace EMBC.Tests.Unit.ESS
+{
+    public class MappingTests
+    {
+        private readonly MapperConfiguration mapperConfig;
+        private IMapper mapper => mapperConfig.CreateMapper();
+
+        public MappingTests()
+        {
+            //var locationManager = A.Fake<ILocationManager>();
+            //A.CallTo(() => locationManager.GetJurisdictions("CAN", "BC", null)).Returns(FakeGenerator.Jurisdictions);
+            mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(typeof(Startup));
+                //cfg.ConstructServicesUsing(t => t switch
+                //{
+                //    Type st when st == typeof(BcscCityConverter) => new BcscCityConverter(locationManager),
+                //    Type st => Activator.CreateInstance(st),
+                //    _ => null
+                //});
+            });
+        }
+
+        [Fact]
+        public void ValidateAutoMapperMappings()
+        {
+            mapperConfig.AssertConfigurationIsValid();
+        }
+    }
+}
