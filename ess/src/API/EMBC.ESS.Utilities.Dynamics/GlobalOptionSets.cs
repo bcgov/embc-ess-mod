219380815868b0178274d17cae8558c9df111867
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-03-16 17:06:37 -0700

    Updated team member mapping and enabled soft delete

---
+++ ess/src/API/EMBC.ESS.Utilities.Dynamics/GlobalOptionSets.cs
@@ -0,0 +1,15 @@
+﻿namespace EMBC.ESS.Utilities.Dynamics
+{
+    public enum EntityStatus
+    {
+        Active = 1,
+        Inactive = 2,
+        SoftDelete = 174360000
+    }
+
+    public enum EntityState
+    {
+        Active = 0,
+        Inactive = 1
+    }
+}
