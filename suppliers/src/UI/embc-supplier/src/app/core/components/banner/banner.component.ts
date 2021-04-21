44b6611eacb307ac8da4a4033166401e786e1ed3
Mike Shay <mshay@filmrobot.com>
2021-04-21 11:35:18 -0700

    Changed banner to allow styling from controlled sources, fixed default Maintenance Page message

--- suppliers/src/UI/embc-supplier/src/app/core/components/banner/banner.component.ts
+++ suppliers/src/UI/embc-supplier/src/app/core/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-banner',
@@ -6,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-  @Input() bannerMsg: string;
+  @Input() bannerMsg: string | SafeHtml;
   @Input() bannerType: string;
 
   constructor() { }
