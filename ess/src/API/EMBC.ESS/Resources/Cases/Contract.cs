4b66ea7f414172addf06a430239a933e5a93162d
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-04-23 12:55:32 -0700

    added case and profile interfaces

---
+++ ess/src/API/EMBC.ESS/Resources/Cases/Contract.cs
@@ -0,0 +1,107 @@
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2021 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EMBC.ESS.Resources.Evacuation
+{
+    public interface ICaseRepository
+    {
+        Task<string> Create(Case @case);
+
+        Task<IEnumerable<Case>> Read(string caseId);
+
+        Task<string> Update(Case @case);
+
+        Task<bool> Delete(string caseId);
+    }
+
+    public abstract class Case
+    {
+        public string Id { get; set; }
+    }
+
+    public class EvacuationFile : Case
+    {
+        public EvacuationAddress EvacuatedFromAddress { get; set; }
+        public IEnumerable<NeedsAssessment> NeedsAssessments { get; set; } = Array.Empty<NeedsAssessment>();
+    }
+
+    public class EvacuationAddress
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Jurisdiction { get; set; }
+        public string StateProvince { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+
+    public class NeedsAssessment
+    {
+        public string Id { get; set; }
+
+        public InsuranceOption Insurance { get; set; }
+
+        public bool? CanEvacueeProvideFood { get; set; }
+        public bool? CanEvacueeProvideLodging { get; set; }
+        public bool? CanEvacueeProvideClothing { get; set; }
+        public bool? CanEvacueeProvideTransportation { get; set; }
+        public bool? CanEvacueeProvideIncidentals { get; set; }
+        public bool HaveSpecialDiet { get; set; }
+        public string SpecialDietDetails { get; set; }
+        public bool HaveMedication { get; set; }
+        public IEnumerable<HouseholdMember> HouseholdMembers { get; set; } = Array.Empty<HouseholdMember>();
+        public IEnumerable<Pet> Pets { get; set; } = Array.Empty<Pet>();
+        public bool? HasPetsFood { get; set; }
+        public NeedsAssessmentType Type { get; set; }
+    }
+
+    public class HouseholdMember
+    {
+        public string Id { get; set; }
+        public PersonDetails Details { get; set; }
+        public bool isUnder19 { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Initials { get; set; }
+        public string PreferredName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+
+    public class Pet
+    {
+        public string Type { get; set; }
+        public string Quantity { get; set; }
+    }
+
+    public enum InsuranceOption
+    {
+        No,
+        Yes,
+        Unsure,
+        Unknown
+    }
+
+    public enum NeedsAssessmentType
+    {
+        Preliminary,
+        Assessed
+    }
+}
---
+++ ess/src/API/EMBC.ESS/Resources/Profiles/Contract.cs
@@ -0,0 +1,76 @@
+﻿// -------------------------------------------------------------------------
+//  Copyright © 2021 Province of British Columbia
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace EMBC.ESS.Resources.Profiles
+{
+    public interface IProfileRepository
+    {
+        Task<string> Create(Profile profile);
+
+        Task<Profile> Read(string profileId);
+
+        Task Update(Profile profile);
+
+        Task Delete(string profileId);
+    }
+
+    public class Profile
+    {
+        public PersonDetails PersonalDetails { get; set; }
+        public ContactDetails ContactDetails { get; set; }
+        public Address PrimaryAddress { get; set; }
+        public Address MailingAddress { get; set; }
+        public bool IsMailingAddressSameAsPrimaryAddress { get; set; }
+        public bool RestrictedAccess { get; set; }
+        public string SecretPhrase { get; set; }
+    }
+
+    public class Address
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Jurisdiction { get; set; }
+        public string StateProvince { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+
+    /// <summary>
+    /// Profile personal details
+    /// </summary>
+    public class PersonDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Initials { get; set; }
+        public string PreferredName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+
+    /// <summary>
+    /// Profile contact information
+    /// </summary>
+    public class ContactDetails
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool HidePhoneRequired { get; set; }
+        public bool HideEmailRequired { get; set; }
+    }
+}
