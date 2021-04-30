5c626944701f1072bdefd4e3a3b270c94b94b2ad
Mike Shay <mshay@filmrobot.com>
2021-04-30 13:23:25 -0700

    Updated Alert component on Supplier portal to match Responder portal Added Error page and routing, needs automatic routing for failed Config check or API error

---
+++ suppliers/src/UI/embc-supplier/src/app/error/error-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error.component';
+
+const routes: Routes = [
+    {path: '', component: ErrorComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ErrorRoutingModule { }
