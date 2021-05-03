8652c2c05e64dc53581049bfcfea64a7988c2901
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-05-03 16:56:30 -0700

    updated

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction-routing.module.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestrictionComponent } from './restriction.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: RestrictionComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction.module.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RestrictionRoutingModule } from './restriction-routing.module';
 import { RestrictionComponent } from './restriction.component';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [RestrictionComponent],
-  imports: [CommonModule, RestrictionRoutingModule]
+  imports: [CommonModule, RestrictionRoutingModule, MaterialModule]
 })
 export class RestrictionModule {}
