a9c9e54144b41f252c2611e908ec4793a10378c5
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-05-04 09:32:13 -0700

    test cases update

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/collection-notice/collection-notice.component.spec.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/collection-notice/collection-notice.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollectionNoticeComponent } from './collection-notice.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CollectionNoticeComponent', () => {
   let component: CollectionNoticeComponent;
@@ -8,6 +9,7 @@ describe('CollectionNoticeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [CollectionNoticeComponent]
     }).compileComponents();
   });
