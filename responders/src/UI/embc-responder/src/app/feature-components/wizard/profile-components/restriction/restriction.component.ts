6735cd39c2920003dfd007153f1b5c2a1f33fec7
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-05-06 17:38:10 -0700

    lint issue

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/restriction/restriction.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { StepCreateProfileService } from '../../step-create-profile/step-create-profile.service';
 
@@ -8,18 +14,38 @@ import { StepCreateProfileService } from '../../step-create-profile/step-create-
   styleUrls: ['./restriction.component.scss']
 })
 export class RestrictionComponent implements OnInit {
+  restrictionForm: FormGroup;
+
   constructor(
     private router: Router,
-    private stepCreateProfileService: StepCreateProfileService
+    private stepCreateProfileService: StepCreateProfileService,
+    private formBuilder: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createRestrictionForm();
+  }
+
+  createRestrictionForm(): void {
+    this.restrictionForm = this.formBuilder.group({
+      restrictedAccess: ['', [Validators.required]]
+    });
+  }
+
+  /**
+   * Returns the control of the form
+   */
+  get restrFormControl(): { [key: string]: AbstractControl } {
+    return this.restrictionForm.controls;
+  }
 
   /**
    * Updates the tab status and navigate to next tab
    */
   next(): void {
     this.stepCreateProfileService.setTabStatus('restriction', 'complete');
-    this.router.navigate(['/ess-wizard/create-evacuee-profile/security-questions']);
+    this.router.navigate([
+      '/ess-wizard/create-evacuee-profile/security-questions'
+    ]);
   }
 }
