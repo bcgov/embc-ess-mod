5d5cdb5c4d7cd24d1020970696f3edbf70e30cd2
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-05-12 09:52:53 -0700

    EMBCESSMOD-1467: Shared address components
    - BC address
    - CAN address
    - USA address
    - Other address

---
+++ responders/src/UI/embc-responder/src/app/shared/forms/address-forms/can-address/can-address.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { CanAddressComponent } from './can-address.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+describe('CanAddressComponent', () => {
+  let component: CanAddressComponent;
+  let fixture: ComponentFixture<CanAddressComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, MatAutocompleteModule ],
+      declarations: [ CanAddressComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CanAddressComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
