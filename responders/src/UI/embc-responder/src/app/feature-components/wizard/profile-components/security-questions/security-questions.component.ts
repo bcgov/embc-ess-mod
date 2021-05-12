58353c091521fb3c24e46d89fbce222c09ca2766
Mike Shay <mshay@filmrobot.com>
2021-05-12 00:29:12 -0700

    Started Security Question page body, added Bold to stylesheet, hooked up Bypass button, temporary hardcoded questions

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/security-questions/security-questions.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/security-questions/security-questions.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { StepCreateProfileService } from '../../step-create-profile/step-create-profile.service';
 import * as globalConst from '../../../../core/services/global-constants';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-security-questions',
@@ -9,18 +12,53 @@ import * as globalConst from '../../../../core/services/global-constants';
   styleUrls: ['./security-questions.component.scss']
 })
 export class SecurityQuestionsComponent implements OnInit {
+  evacueeDetailsForm: FormGroup = null;
+
+  bypassQuestions = false;
+
+  secQuestions = [
+    'What was the name of your first pet?',
+    'What was your first car’s make and model? (e.g. Ford Taurus)',
+    'Where was your first job?',
+    'What is your favourite children\'s book?',
+    'In what city or town was your mother born?',
+    'What is your favourite movie?',
+    'What is your oldest sibling\'s middle name?',
+    'What month and day is your anniversary?',
+    'What was your childhood nickname?',
+    'What were the last four digits of your childhood telephone number?',
+    'In what town or city did you meet your spouse or partner?'
+  ];
+
   constructor(
     private router: Router,
     private stepCreateProfileService: StepCreateProfileService
   ) {}
 
   ngOnInit(): void {}
+  
+  get evacueeFormControl(): { [key: string]: AbstractControl; } {
+    return this.evacueeDetailsForm?.controls;
+  }
+
+  changeBypass(event:MatCheckboxChange) {
+    this.bypassQuestions = event.checked;
+
+    if (!this.bypassQuestions) {
+      // Reset dropdowns/inputs
+    }
+  }
+
+  back(): void {
+
+  }
 
   next(): void {
     this.stepCreateProfileService.setTabStatus(
       'security-questions',
       'complete'
     );
+
     if (this.stepCreateProfileService.checkTabsStatus()) {
       this.stepCreateProfileService.openModal(globalConst.wizardProfileMessage);
     } else {
--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/security-questions/security-questions.module.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/security-questions/security-questions.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 import { SecurityQuestionsRoutingModule } from './security-questions-routing.module';
 import { SecurityQuestionsComponent } from './security-questions.component';
 
 @NgModule({
-  declarations: [SecurityQuestionsComponent],
-  imports: [CommonModule, SecurityQuestionsRoutingModule]
+  declarations: [
+    SecurityQuestionsComponent
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    SecurityQuestionsRoutingModule
+  ]
 })
 export class SecurityQuestionsModule {}
