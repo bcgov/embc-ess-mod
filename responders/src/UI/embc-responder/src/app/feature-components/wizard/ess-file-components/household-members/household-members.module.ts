5486a52cbac32a41083e2179e13b9654daee674a
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2021-05-14 15:13:33 -0700

    changing household member component declaration

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/ess-file-components/household-members/household-members.module.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/ess-file-components/household-members/household-members.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HouseholdMembersRoutingModule } from './household-members-routing.module';
+import { HouseholdMembersComponent } from './household-members.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HouseholdMembersComponent],
   imports: [CommonModule, HouseholdMembersRoutingModule]
 })
 export class HouseholdMembersModule {}
--- responders/src/UI/embc-responder/src/app/feature-components/wizard/wizard.module.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/wizard.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { WizardRoutingModule } from './wizard-routing.module';
 import { WizardComponent } from './wizard.component';
 import { MaterialModule } from 'src/app/material.module';
-import { HouseholdMembersComponent } from './ess-file-components/household-members/household-members.component';
 
 @NgModule({
-  declarations: [WizardComponent, HouseholdMembersComponent],
+  declarations: [WizardComponent],
   imports: [CommonModule, WizardRoutingModule, MaterialModule]
 })
 export class WizardModule {}
