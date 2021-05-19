7bff6dc6587fba6b0f5099bcc6647860132605ae
Mike Shay <mshay@filmrobot.com>
2021-05-19 15:20:25 -0700

    Got most of the Review & Save tab set up

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/profile-review/profile-review.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/profile-components/profile-review/profile-review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Profile } from 'src/app/core/models/profile';
 import { StepCreateProfileService } from '../../step-create-profile/step-create-profile.service';
 import { WizardService } from '../../wizard.service';
 
@@ -12,19 +13,28 @@ export class ProfileReviewComponent implements OnInit {
   constructor(
     private router: Router,
     private wizardService: WizardService,
-    private stepCreateProfileService: StepCreateProfileService
+    public stepCreateProfileService: StepCreateProfileService
   ) {}
 
   ngOnInit(): void {}
+
+  /**
+   * Go back to the Security Questions tab
+   */
+  back(): void {
+    this.router.navigate([
+      '/ess-wizard/create-evacuee-profile/security-questions'
+    ]);
+  }
 
   /**
    * Updates the tab status, step status and navigates
    * to the next step
    */
   save(): void {
-    console.log('in save');
     this.stepCreateProfileService.setTabStatus('review', 'complete');
     this.wizardService.setStepStatus('/ess-wizard/create-ess-file', false);
+
     this.router.navigate(['/ess-wizard/create-ess-file'], {
       state: { step: 'STEP 2', title: 'Create ESS File' }
     });
