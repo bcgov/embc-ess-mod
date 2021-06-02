0888f7b8233dbcef74013a0ecf6991f77c1f380b
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-06-02 15:18:20 -0700

    EMBCESSMOD-1715

--- responders/src/UI/embc-responder/src/app/feature-components/search/evacuee-profile-dashboard/possible-matched-essfiles/possible-matched-essfiles.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/search/evacuee-profile-dashboard/possible-matched-essfiles/possible-matched-essfiles.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-possible-matched-essfiles',
@@ -6,7 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./possible-matched-essfiles.component.scss']
 })
 export class PossibleMatchedEssfilesComponent implements OnInit {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  currentlyOpenedItemIndex = -1;
+
+  essFiles = [
+    {
+      fileNumber: '12345',
+      createdDate: '20-Mar-2021',
+      evacuatedFrom: 'something',
+      status: 'pending'
+    }
+  ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**
+   * Sets expanded input value for panel
+   *
+   * @param index
+   * @returns
+   */
+  isExpanded(index: number): boolean {
+    return index === 0;
+  }
+
+  /**
+   * Updates value of openend file index
+   *
+   * @param itemIndex selected file index
+   */
+  setOpened(itemIndex: number): void {
+    this.currentlyOpenedItemIndex = itemIndex;
+  }
+
+  /**
+   * Resets the opened file index to default
+   *
+   * @param itemIndex closed file index
+   */
+  setClosed(itemIndex: number): void {
+    if (this.currentlyOpenedItemIndex === itemIndex) {
+      this.currentlyOpenedItemIndex = -1;
+    }
+  }
 }
