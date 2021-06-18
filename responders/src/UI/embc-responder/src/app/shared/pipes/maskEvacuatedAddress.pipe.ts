e6b34ffc786767956f7e8291daee6f9f93f5d321
Mike Shay <mshay@filmrobot.com>
2021-06-18 12:14:40 -0700

    Community/city name lookup now gets names again

--- responders/src/UI/embc-responder/src/app/shared/pipes/maskEvacuatedAddress.pipe.ts
+++ responders/src/UI/embc-responder/src/app/shared/pipes/maskEvacuatedAddress.pipe.ts
@@ -28,15 +28,7 @@ export class MaskEvacuatedAddressPipe implements PipeTransform {
       if (address.addressLine2?.length > 0)
         line1 += ', ' + address.addressLine2;
 
-      // TODO: Rewrite this once the new community code mapping is merged
-      const communityName = address.community?.code || '';
-
-      // const communityName =
-      //   communities.find((community) => {
-      //     return community.code === address.community?.code;
-      //   })?.name ||
-      //   address.city ||
-      //   '';
+      const communityName = address.community?.name ?? address.city ?? '';
 
       // Only set line 2 if city exists
       if (communityName.length > 0) {
