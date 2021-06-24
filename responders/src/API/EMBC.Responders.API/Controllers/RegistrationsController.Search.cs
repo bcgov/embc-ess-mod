33b4270b29a7800610b0a5e92b1408c5fbdb0ee8
Joseph Weinkam <jweinkam@filmrobot.com>
2021-06-24 14:20:13 -0700

    remove redundant mapping I put in the last commit

--- responders/src/API/EMBC.Responders.API/Controllers/RegistrationsController.Search.cs
+++ responders/src/API/EMBC.Responders.API/Controllers/RegistrationsController.Search.cs
@@ -111,7 +111,6 @@ namespace EMBC.Responders.API.Controllers
             CreateMap<RegistrantWithFiles, RegistrantProfileSearchResult>()
                 .IncludeMembers(s => s.RegistrantProfile)
                 .ForMember(d => d.EvacuationFiles, opts => opts.MapFrom(s => s.Files))
-                .ForMember(d => d.ModifiedOn, opts => opts.MapFrom(s => s.RegistrantProfile.LastModified))
                 ;
 
             CreateMap<ESS.Shared.Contracts.Submissions.RegistrantProfile, RegistrantProfileSearchResult>()
