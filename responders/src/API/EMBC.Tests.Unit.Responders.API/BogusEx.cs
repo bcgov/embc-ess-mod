3a6fa34f26223e1af45a8787832bf0a2669a3be1
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-06-30 16:50:32 -0700

    evacuation search bug fixing and unit tests (WIP)

---
+++ responders/src/API/EMBC.Tests.Unit.Responders.API/BogusEx.cs
@@ -0,0 +1,9 @@
+﻿using Bogus;
+
+namespace EMBC.Tests.Unit.Responders.API
+{
+    public static class BogusEx
+    {
+        public static bool? NullableBool(this Randomizer randomizer) => randomizer.ArrayElement(new[] { (bool?)null, true, false });
+    }
+}
