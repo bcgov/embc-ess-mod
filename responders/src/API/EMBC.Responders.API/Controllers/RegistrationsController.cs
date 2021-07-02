b16df9b14ebb095e4939457e68b9bb387f990838
Joseph Weinkam <jweinkam@filmrobot.com>
2021-07-02 14:06:12 -0700

    add dummy endpoint to create an evacuation file note

--- responders/src/API/EMBC.Responders.API/Controllers/RegistrationsController.cs
+++ responders/src/API/EMBC.Responders.API/Controllers/RegistrationsController.cs
@@ -203,6 +203,26 @@ namespace EMBC.Responders.API.Controllers
         }
 
         /// <summary>
+        /// Create a File Note
+        /// </summary>
+        /// <param name="fileId">fileId</param>
+        /// <param name="note">note</param>
+        /// <returns>newly created note id</returns>
+        [HttpPost("files/{fileId}/note")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<RegistrationResult>> CreateFileNote(string fileId, Note note)
+        {
+            //var id = await messagingClient.Send(new SubmitEvacuationFileCommand
+            //{
+            //    File = mapper.Map<ESS.Shared.Contracts.Submissions.EvacuationFile>(note)
+            //});
+
+            //return Ok(new RegistrationResult { Id = id });
+            return Ok(await Task.FromResult(new RegistrationResult { Id = "note123" }));
+        }
+
+        /// <summary>
         /// get the security phrase of an evacuation file
         /// </summary>
         /// <param name="fileId">file id</param>
