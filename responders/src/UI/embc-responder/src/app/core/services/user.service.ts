c3c9150e0a7b5bcc2d3363a4cfc1466ca8288fb5
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-07-14 15:20:41 -0700

    added svg's

--- responders/src/UI/embc-responder/src/app/core/services/user.service.ts
+++ responders/src/UI/embc-responder/src/app/core/services/user.service.ts
@@ -69,7 +69,6 @@ export class UserService {
     this.cacheService.remove('memberLabels');
     this.cacheService.remove('allTeamCommunityList');
     this.cacheService.remove('teamCommunityList');
-    this.cacheService.remove('evacueeSearchContext');
     this.cacheService.remove('stateProvinceList');
     this.cacheService.remove('wizardOpenedFrom');
     this.cacheService.remove('regionalDistrictsList');
