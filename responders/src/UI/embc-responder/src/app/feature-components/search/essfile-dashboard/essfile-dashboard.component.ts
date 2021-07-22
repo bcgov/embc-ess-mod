5e5cc485ef5886155b89a190ab21ff628639053a
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-07-22 14:18:52 -0700

    EMBCESSMOD-2117, 2118

--- responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/essfile-dashboard.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/essfile-dashboard.component.ts
@@ -12,6 +12,9 @@ import { FileStatusDefinitionComponent } from 'src/app/shared/components/dialog-
 import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 import { EssfileDashboardService } from './essfile-dashboard.service';
 import * as globalConst from '../../../core/services/global-constants';
+import { Note } from 'src/app/core/api/models';
+import { StepNotesService } from '../../wizard/step-notes/step-notes.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-essfile-dashboard',
@@ -20,6 +23,7 @@ import * as globalConst from '../../../core/services/global-constants';
 })
 export class EssfileDashboardComponent implements OnInit {
   essFile: EvacuationFileModel;
+  notesList: Array<Note>;
   isLoading = false;
   color = '#169BD5';
 
@@ -29,7 +33,8 @@ export class EssfileDashboardComponent implements OnInit {
     private dialog: MatDialog,
     private router: Router,
     private essfileDashboardService: EssfileDashboardService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private stepNotesService: StepNotesService
   ) {}
 
   ngOnInit(): void {
@@ -97,17 +102,34 @@ export class EssfileDashboardComponent implements OnInit {
     this.isLoading = !this.isLoading;
     this.essFileService
       .getFileFromId(this.evacueeSessionService.essFileNumber)
-      .subscribe(
-        (file) => {
-          this.isLoading = !this.isLoading;
+      .pipe(
+        map((file) => {
+          this.loadNotes();
           this.essFile = file;
           this.essfileDashboardService.essFile = file;
           this.loadDefaultOverviewSection(file);
+        })
+      )
+      .subscribe(
+        (essFile) => {
+          this.isLoading = !this.isLoading;
         },
         (error) => {
           this.isLoading = !this.isLoading;
           this.alertService.setAlert('danger', globalConst.fileDashboardError);
         }
       );
+  }
+
+  /**
+   * Loads and sorts the notes
+   */
+  private loadNotes(): void {
+    this.stepNotesService.getNotes().subscribe((notes) => {
+      const note = notes.sort(
+        (a, b) => new Date(b.addedOn).valueOf() - new Date(a.addedOn).valueOf()
+      );
+      this.notesList = note;
+    });
   }
 }
