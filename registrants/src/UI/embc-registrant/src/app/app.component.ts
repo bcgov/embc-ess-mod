b963c190ed78e95e876255275b8e9d23f79b03f3
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-08-11 15:14:49 -0700

    EMBCESSMOD-2344
    changed loading of static location lists

--- registrants/src/UI/embc-registrant/src/app/app.component.ts
+++ registrants/src/UI/embc-registrant/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiConfiguration } from './core/api/api-configuration';
+import { LocationService } from './core/services/location.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,18 @@ import { ApiConfiguration } from './core/api/api-configuration';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(apiConfig: ApiConfiguration) {
+  constructor(
+    apiConfig: ApiConfiguration,
+    private locationService: LocationService
+  ) {
     apiConfig.rootUrl = '';
+    this.loadStaticLists();
+  }
+
+  private loadStaticLists(): void {
+    this.locationService.getCommunityList();
+    this.locationService.getCountriesList();
+    this.locationService.getRegionalDistricts();
+    this.locationService.getStateProvinceList();
   }
 }
