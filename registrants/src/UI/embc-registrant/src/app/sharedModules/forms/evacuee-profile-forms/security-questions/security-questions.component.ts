a038cb41b168f0847dac117d608426eabdd0891b
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-08-12 16:57:31 -0700

    Update security-questions.component.ts

--- registrants/src/UI/embc-registrant/src/app/sharedModules/forms/evacuee-profile-forms/security-questions/security-questions.component.ts
+++ registrants/src/UI/embc-registrant/src/app/sharedModules/forms/evacuee-profile-forms/security-questions/security-questions.component.ts
@@ -3,15 +3,13 @@ import {
   AbstractControl,
   FormBuilder,
   FormGroup,
-  ReactiveFormsModule,
-  Validators
+  ReactiveFormsModule
 } from '@angular/forms';
 
-import { CustomValidationService } from 'src/app/core/services/customValidation.service';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormCreationService } from 'src/app/core/services/formCreation.service';
 import { MatSelectModule } from '@angular/material/select';
@@ -35,7 +33,6 @@ export default class SecurityQuestionsComponent implements OnInit {
   constructor(
     @Inject('formBuilder') formBuilder: FormBuilder,
     @Inject('formCreationService') formCreationService: FormCreationService,
-    private customValidationService: CustomValidationService,
     private securityQuesService: SecurityQuestionsService,
     private alertService: AlertService
   ) {
@@ -59,55 +56,12 @@ export default class SecurityQuestionsComponent implements OnInit {
    * Set up main FormGroup with security Q&A inputs and validation
    */
   createQuestionForm(): void {
-    //this.setQuestionArray(false);
-
     this.securityQuestionsForm$ = this.formCreationService
       .getSecurityQuestionsForm()
       .subscribe((securityQuestionsForm) => {
         console.log(securityQuestionsForm);
         this.securityQuestionsForm = securityQuestionsForm;
       });
-
-    // this.questionForm = this.formBuilder.group(
-    //   {
-    //     question1: ['', [Validators.required]],
-    //     answer1: [
-    //       '',
-    //       [
-    //         Validators.minLength(3),
-    //         Validators.maxLength(50),
-    //         this.customValidationService.whitespaceValidator()
-    //       ]
-    //     ],
-    //     question2: ['', [Validators.required]],
-    //     answer2: [
-    //       '',
-    //       [
-    //         Validators.minLength(3),
-    //         Validators.maxLength(50),
-    //         this.customValidationService.whitespaceValidator()
-    //       ]
-    //     ],
-    //     question3: ['', [Validators.required]],
-    //     answer3: [
-    //       '',
-    //       [
-    //         Validators.minLength(3),
-    //         Validators.maxLength(50),
-    //         this.customValidationService.whitespaceValidator()
-    //       ]
-    //     ]
-    //   }
-    // {
-    //   validators: [
-    //     this.customValidationService.uniqueValueValidator([
-    //       'question1',
-    //       'question2',
-    //       'question3'
-    //     ])
-    //   ]
-    // }
-    //);
   }
 
   get securityQuestionsFormControl(): { [key: string]: AbstractControl } {
@@ -117,44 +71,6 @@ export default class SecurityQuestionsComponent implements OnInit {
   get questionsFormControl(): { [key: string]: AbstractControl } {
     return (this.securityQuestionsForm.get('questions') as FormGroup).controls;
   }
-
-  /**
-   * Make sure that the securityQuestion array is set to the correct length
-   *
-   * @param clear If true, reset securityQuestions to empty/default values
-   */
-  // setQuestionArray(clear) {
-  //   if (!this.stepEvacueeProfileService.securityQuestions || clear)
-  //     this.stepEvacueeProfileService.securityQuestions = [];
-
-  //   // Set up 3 blank security question values if not already there
-  //   while (this.stepEvacueeProfileService.securityQuestions.length < 3) {
-  //     this.stepEvacueeProfileService.securityQuestions.push({
-  //       id: this.stepEvacueeProfileService.securityQuestions.length + 1,
-  //       question: '',
-  //       answer: ''
-  //     });
-  //   }
-  // }
-
-  // /**
-  //  * Switches security question tab between readonly mode and editable mode
-  //  *
-  //  * @param edit True to set "Edit" mode, False to set "Readonly" mode and clear form
-  //  */
-  // toggleEditQuestions(edit) {
-  //   this.stepEvacueeProfileService.editQuestions = edit;
-
-  //   if (!edit) {
-  //     // Reset bypass Questions if selected
-  //     this.stepEvacueeProfileService.bypassSecurityQuestions = false;
-  //     this.setFormDisabled(false);
-
-  //     // Clear now-hidden Q&A form
-  //     this.setQuestionArray(true);
-  //     this.questionForm.reset();
-  //   }
-  // }
 }
 
 @NgModule({
