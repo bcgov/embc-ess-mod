46e754c7491ab435f523b3f634a833b76d5658a3
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-08-18 14:22:09 -0700

    auto gen changes

---
+++ responders/src/UI/embc-responder/src/app/core/api/models/lodging-hotel-referral.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+import { Referral } from './referral';
+import { SupportCategory } from './support-category';
+import { SupportSubCategory } from './support-sub-category';
+export interface LodgingHotelReferral extends Referral {
+  category?: SupportCategory;
+  numberOfNights: number;
+  numberOfRooms: number;
+  subCategory?: SupportSubCategory;
+}
