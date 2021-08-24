645dc9dd59bd3be1fb3c5d6c9c1c5d5cdf8d1c0f
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-08-24 15:27:09 -0700

    EMBCESSMOD-2384, 2500

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/view-supports/view-supports.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/view-supports/view-supports.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { Router } from '@angular/router';
+import { Support } from 'src/app/core/api/models';
+import { TableFilterValueModel } from 'src/app/core/models/table-filter-value.model';
+import { TableFilterModel } from 'src/app/core/models/table-filter.model';
+import { StepSupportsService } from '../../step-supports/step-supports.service';
+import { ViewSupportsService } from './view-supports.service';
 
 @Component({
   selector: 'app-view-supports',
@@ -7,11 +13,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-supports.component.scss']
 })
 export class ViewSupportsComponent implements OnInit {
-  constructor(private router: Router) {}
+  supportList: Support[];
+  filterTerm: TableFilterValueModel;
+  filtersToLoad: TableFilterModel;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    public stepSupportsService: StepSupportsService,
+    private viewSupportsService: ViewSupportsService
+  ) {}
+
+  ngOnInit(): void {
+    this.supportList = this.stepSupportsService.existingSupportList.sort(
+      (a, b) => new Date(b.from).valueOf() - new Date(a.from).valueOf()
+    );
+
+    this.filtersToLoad = this.viewSupportsService.load();
+  }
 
   addSupports() {
     this.router.navigate(['/ess-wizard/add-supports/select-support']);
+  }
+
+  process() {}
+
+  selected(event: MatSelectChange, filterType: string): void {
+    let selectedValue =
+      event.value === undefined || event.value === ''
+        ? ''
+        : event.value.description;
+    this.filterTerm = { type: filterType, value: selectedValue };
   }
 }
---
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/view-supports/view-supports.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Code } from 'src/app/core/api/models';
+import { SupportStatus } from 'src/app/core/api/models/support-status';
+import {
+  ObjectWrapper,
+  TableFilterModel
+} from 'src/app/core/models/table-filter.model';
+import { StepSupportsService } from '../../step-supports/step-supports.service';
+
+@Injectable({ providedIn: 'root' })
+export class ViewSupportsService {
+  typesList: Code[] = this.stepSupportsService.supportCategory;
+  defaultType: ObjectWrapper = {
+    code: 'Support Type',
+    description: 'All Support Types'
+  };
+
+  defaultStatus: ObjectWrapper = {
+    code: 'All Status',
+    description: 'All Status'
+  };
+
+  constructor(public stepSupportsService: StepSupportsService) {}
+
+  public load(): TableFilterModel {
+    return {
+      loadDropdownFilters: [
+        {
+          type: 'type',
+          label: this.defaultType,
+          values: this.stepSupportsService.supportCategory
+        },
+        {
+          type: 'status',
+          label: this.defaultStatus,
+          values: this.statusList()
+        }
+      ]
+    };
+  }
+
+  statusList() {
+    console.log(this.typesList);
+    let status = [];
+    for (const [propertyKey, propertyValue] of Object.entries(SupportStatus)) {
+      if (!Number.isNaN(Number(propertyKey))) {
+        continue;
+      }
+      status.push({ code: propertyValue, description: propertyKey });
+    }
+    return status;
+  }
+}
