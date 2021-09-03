6977ed343b0062799dd47b176a0873e64367c20a
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-09-03 13:22:54 -0700

    empty support components
    -transportation
    -lodging

---
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/support-details/details-type/lodging-group/lodging-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lodging-group',
+  templateUrl: './lodging-group.component.html',
+  styleUrls: ['./lodging-group.component.scss']
+})
+export class LodgingGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/support-details/details-type/lodging-hotel-motel/lodging-hotel-motel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lodging-hotel-motel',
+  templateUrl: './lodging-hotel-motel.component.html',
+  styleUrls: ['./lodging-hotel-motel.component.scss']
+})
+export class LodgingHotelMotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
