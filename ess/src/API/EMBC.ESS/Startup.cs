f92db4fac120e3f2107104e0b1f9e5377b4e3a18
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-09-08 07:43:16 -0700

    ess backend streamlined logging and health checks

--- ess/src/API/EMBC.ESS/Startup.cs
+++ ess/src/API/EMBC.ESS/Startup.cs
@@ -14,6 +14,8 @@
 //  limitations under the License.
 // -------------------------------------------------------------------------
 
+using System;
+using System.Net;
 using EMBC.ESS.Engines.Search;
 using EMBC.ESS.Managers.Admin;
 using EMBC.ESS.Managers.Metadata;
@@ -32,15 +34,21 @@ using EMBC.ESS.Utilities.Transformation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace EMBC.ESS
 {
     public class Startup
     {
+        private const string HealthCheckReadyTag = "ready";
+        private const string HealthCheckAliveTag = "alive";
+
         private readonly IConfiguration configuration;
 
         public Startup(IConfiguration configuration)
@@ -59,7 +67,9 @@ namespace EMBC.ESS
                 opts.EnableDetailedErrors = true;
             });
 
-            services.AddHealthChecks().AddCheck("ESS Backend", () => HealthCheckResult.Healthy("ESS Backend OK"), new[] { "ready" });
+            services.AddHealthChecks()
+                .AddCheck("ESS API ready hc", () => HealthCheckResult.Healthy("API ready"), new[] { HealthCheckReadyTag })
+                .AddCheck("ESS API live hc", () => HealthCheckResult.Healthy("API alive"), new[] { HealthCheckAliveTag });
             services.AddAutoMapper((sp, cfg) => { cfg.ConstructServicesUsing(t => sp.GetRequiredService(t)); }, typeof(Startup));
 
             services
@@ -85,29 +95,46 @@ namespace EMBC.ESS
                 .AddNotificationSenders(configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSerilogRequestLogging(opts =>
+            {
+                opts.GetLevel = ExcludeHealthChecks;
+                opts.EnrichDiagnosticContext = (diagCtx, httpCtx) =>
+                {
+                    diagCtx.Set("User", httpCtx.User.Identity?.Name);
+                    diagCtx.Set("Host", httpCtx.Request.Host);
+                    diagCtx.Set("UserAgent", httpCtx.Request.Headers["User-Agent"].ToString());
+                    diagCtx.Set("RemoteIP", httpCtx.Connection.RemoteIpAddress.ToString());
+                    diagCtx.Set("ConnectionId", httpCtx.Connection.Id);
+                    diagCtx.Set("Forwarded", httpCtx.Request.Headers["Forwarded"].ToString());
+                    diagCtx.Set("ContentLength", httpCtx.Response.ContentLength);
+                };
+            });
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<DispatcherService>();
-                endpoints.MapHealthChecks("/hc/ready", new HealthCheckOptions()
-                {
-                    Predicate = (check) => check.Tags.Contains("ready")
-                });
-
-                endpoints.MapHealthChecks("/hc/live", new HealthCheckOptions()
-                {
-                    Predicate = (_) => false
-                });
+                endpoints.MapHealthChecks("/hc/ready", new HealthCheckOptions() { Predicate = check => check.Tags.Contains(HealthCheckReadyTag) });
+                endpoints.MapHealthChecks("/hc/live", new HealthCheckOptions() { Predicate = check => check.Tags.Contains(HealthCheckAliveTag) });
+                endpoints.MapHealthChecks("/hc/startup", new HealthCheckOptions() { Predicate = _ => false });
             });
         }
+
+        //inspired by https://andrewlock.net/using-serilog-aspnetcore-in-asp-net-core-3-excluding-health-check-endpoints-from-serilog-request-logging/
+        private static LogEventLevel ExcludeHealthChecks(HttpContext ctx, double _, Exception ex) =>
+        ex != null
+            ? LogEventLevel.Error
+            : ctx.Response.StatusCode >= (int)HttpStatusCode.InternalServerError
+                ? LogEventLevel.Error
+                : ctx.Request.Path.StartsWithSegments("/hc", StringComparison.InvariantCultureIgnoreCase)
+                    ? LogEventLevel.Verbose
+                    : LogEventLevel.Information;
     }
 }
