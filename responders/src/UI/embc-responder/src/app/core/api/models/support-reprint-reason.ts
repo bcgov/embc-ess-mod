ce25d4d33d0bacdd9d8e89d8da06623348abf7d0
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2021-09-09 11:20:48 -0700

    Revert "api generated changes"
    This reverts commit bbc773319651398cfe4cc344d9d2ba7c775a87b7.

--- responders/src/UI/embc-responder/src/app/core/api/models/support-reprint-reason.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export enum SupportReprintReason {
-  ErrorOnPrintedReferral = 'ErrorOnPrintedReferral',
-  PrintedError = 'PrintedError',
-  EvacueeLostReferral = 'EvacueeLostReferral'
-}
--- responders/src/UI/embc-responder/src/app/core/api/models/support-void-reason.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export enum SupportVoidReason {
-  ErrorOnPrintedReferral = 'ErrorOnPrintedReferral',
-  NewSupplierRequired = 'NewSupplierRequired',
-  SupplierCouldNotMeetNeed = 'SupplierCouldNotMeetNeed'
-}
