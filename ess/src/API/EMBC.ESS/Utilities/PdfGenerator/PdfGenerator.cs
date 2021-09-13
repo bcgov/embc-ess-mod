fcb092c50ea63a16eab6a0a9d1c386b819e7d07e
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-09-13 14:14:31 -0700

    singletone browser

--- ess/src/API/EMBC.ESS/Utilities/PdfGenerator/PdfGenerator.cs
+++ ess/src/API/EMBC.ESS/Utilities/PdfGenerator/PdfGenerator.cs
@@ -26,21 +26,19 @@ namespace EMBC.ESS.Utilities.PdfGenerator
         public Task<byte[]> Generate(string source);
     }
 
-    public class PdfGenerator : IPdfGenerator
+    public class PdfGenerator : IPdfGenerator, IDisposable
     {
         private readonly RevisionInfo puppeteerInfo;
         private readonly ILogger<PdfGenerator> logger;
+        private readonly Browser browser;
+        private bool disposedValue;
 
         public PdfGenerator(RevisionInfo puppeteerInfo, ILogger<PdfGenerator> logger)
         {
             this.puppeteerInfo = puppeteerInfo;
             this.logger = logger;
-        }
-
-        public async Task<byte[]> Generate(string source)
-        {
             logger.LogInformation("Using Puppeteer from {0}", puppeteerInfo.ExecutablePath);
-            using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            browser = Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true,
                 ExecutablePath = puppeteerInfo.ExecutablePath,
@@ -48,18 +46,50 @@ namespace EMBC.ESS.Utilities.PdfGenerator
                 Timeout = TimeSpan.FromSeconds(10).Milliseconds,
                 LogProcess = true,
                 DumpIO = true,
-            });
-            logger.LogInformation("Created Puppeteer browser version {0}", await browser.GetVersionAsync());
+            }).GetAwaiter().GetResult();
+            logger.LogInformation("Created Puppeteer browser version {0}", browser.GetVersionAsync().GetAwaiter().GetResult());
+        }
+
+        public async Task<byte[]> Generate(string source)
+        {
             using var page = await browser.NewPageAsync();
             logger.LogInformation("Created Puppeteer page");
             await page.SetContentAsync(source);
             var content = await page.PdfDataAsync();
             logger.LogInformation("Generated pdf with size {0}", content.Length);
 
-            await browser.CloseAsync();
-            logger.LogInformation("Browser closed");
-
             return content;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    browser.Dispose();
+                    logger.LogInformation("Browser closed");
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~PdfGenerator()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
