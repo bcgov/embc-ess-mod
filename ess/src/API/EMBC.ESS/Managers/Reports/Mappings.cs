0d0e79107c498f4e7ddd9f8aa73e3a9d211215f9
Joseph Weinkam <jweinkam@filmrobot.com>
2021-10-14 12:27:35 -0700

    update evacuee report mapping to be able to hide personal info

--- ess/src/API/EMBC.ESS/Managers/Reports/Mappings.cs
+++ ess/src/API/EMBC.ESS/Managers/Reports/Mappings.cs
@@ -16,6 +16,7 @@
 
 using AutoMapper;
 using EMBC.ESS.Resources.Metadata;
+using EMBC.ESS.Resources.Reports;
 
 namespace EMBC.ESS.Managers.Reports
 {
@@ -23,6 +24,39 @@ namespace EMBC.ESS.Managers.Reports
     {
         public Mappings()
         {
+            CreateMap<Evacuee, Evacuee>()
+                .ForMember(d => d.LastName, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.LastName))
+                .ForMember(d => d.FirstName, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.FirstName))
+                .ForMember(d => d.DateOfBirth, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.DateOfBirth))
+                .ForMember(d => d.Gender, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Gender))
+                .ForMember(d => d.PreferredName, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.PreferredName))
+                .ForMember(d => d.Initials, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Initials))
+                .ForMember(d => d.AddressLine1, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.AddressLine1))
+                .ForMember(d => d.AddressLine2, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.AddressLine2))
+                .ForMember(d => d.Community, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Community))
+                .ForMember(d => d.Province, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Province))
+                .ForMember(d => d.PostalCode, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.PostalCode))
+                .ForMember(d => d.Country, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Country))
+                .ForMember(d => d.Phone, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.Phone))
+                .ForMember(d => d.MailingAddressLine1, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingAddressLine1))
+                .ForMember(d => d.MailingAddressLine2, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingAddressLine2))
+                .ForMember(d => d.MailingCommunity, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingCommunity))
+                .ForMember(d => d.MailingProvince, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingProvince))
+                .ForMember(d => d.MailingPostal, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingPostal))
+                .ForMember(d => d.MailingCountry, opts => opts.ConvertUsing<PersonalInfoConverter, string>(s => s.MailingCountry))
+                ;
+        }
+
+        public class PersonalInfoConverter : IValueConverter<string, string>
+        {
+            public string Convert(string sourceMember, ResolutionContext context)
+            {
+                if (ShouldShowPersonalInfo(context)) return sourceMember;
+                else return string.Empty;
+            }
+
+            public static bool ShouldShowPersonalInfo(ResolutionContext ctx) =>
+                ctx.Options.Items.ContainsKey("IncludePersonalInfo") && bool.Parse(ctx.Options.Items["IncludePersonalInfo"].ToString());
         }
     }
 }
