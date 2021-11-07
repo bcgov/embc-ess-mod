2be9db691996f4cd483cffbd5b08ff49a0029e52
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-11-07 15:14:44 -0800

    fixed angular unit tests

--- registrants/src/UI/embc-registrant/src/app/app.component.spec.ts
+++ registrants/src/UI/embc-registrant/src/app/app.component.spec.ts
@@ -2,13 +2,25 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LoginService } from './core/services/login.service';
+import { BootstrapService } from './core/services/bootstrap.service';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AppComponent', () => {
+  let loginService: jasmine.SpyObj<LoginService>;
+  let bootstrapService: jasmine.SpyObj<BootstrapService>;
+
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule, RouterTestingModule],
-        declarations: [AppComponent]
+        imports: [HttpClientTestingModule, RouterTestingModule, OAuthModule.forRoot()],
+        declarations: [AppComponent],
+        providers: [
+          { provides: LoginService, useValue: loginService },
+          { provides: BootstrapService, useValue: bootstrapService },
+          { provide: APP_BASE_HREF, useValue: '/' }
+        ]
       }).compileComponents();
     })
   );
