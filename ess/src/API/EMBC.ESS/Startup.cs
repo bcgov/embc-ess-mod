218829989d136bf8d65a012b570b8bf5c61202c3
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-11-07 14:51:17 -0800

    fixed build

--- ess/src/API/EMBC.ESS/Startup.cs
+++ ess/src/API/EMBC.ESS/Startup.cs
@@ -46,6 +46,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using Serilog.Events;
 using StackExchange.Redis;
 
