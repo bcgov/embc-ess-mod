36bab507e538f2dea0d602283554f21c84cb957c
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2021-12-10 14:32:28 -0800

    EMBCESSMOD-3089

--- responders/src/UI/embc-responder/src/app/shared/outage-components/outage-dialog/outage-dialog.component.spec.ts
+++ responders/src/UI/embc-responder/src/app/shared/outage-components/outage-dialog/outage-dialog.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
 
 import { OutageDialogComponent } from './outage-dialog.component';
 
@@ -8,7 +13,12 @@ describe('OutageDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [OutageDialogComponent]
+      imports: [MatDialogModule],
+      declarations: [OutageDialogComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     }).compileComponents();
   });
 
--- responders/src/UI/embc-responder/src/app/shared/outage-components/outage-dialog/outage-dialog.component.ts
+++ responders/src/UI/embc-responder/src/app/shared/outage-components/outage-dialog/outage-dialog.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { OutageInformation } from 'src/app/core/api/models';
 
 @Component({
   selector: 'app-outage-dialog',
   templateUrl: './outage-dialog.component.html',
   styleUrls: ['./outage-dialog.component.scss']
 })
-export class OutageDialogComponent implements OnInit {
-  constructor() {}
+export class OutageDialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { message: OutageInformation; time: string },
+    public dialogRef: MatDialogRef<OutageDialogComponent>
+  ) {}
 
-  ngOnInit(): void {}
+  public close(): void {
+    this.dialogRef.close();
+  }
 }
