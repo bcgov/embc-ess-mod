1b15df6e91fb15815349978dd317a48b19e773df
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-01-04 09:31:16 -0800

    test updating shared code

--- shared/src/EMBC.Utilities.Hosting/Host.cs
+++ shared/src/EMBC.Utilities.Hosting/Host.cs
@@ -50,7 +50,7 @@ namespace EMBC.Utilities.Hosting
     /// - AutoMapper helper services
     /// - default routing and controllers
     /// - discover and invoke IConfigureComponentServices, IConfigureComponentPipeline, IHaveGrpcServices implementations in dependant assemblies
-    /// testing symlink changes, now with /D
+    /// testing symlink changes, now with /J
     /// </summary>
     public class Host
     {
