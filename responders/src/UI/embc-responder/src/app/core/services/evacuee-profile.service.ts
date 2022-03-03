f94f3e2742e2f1370e7593f586cd6c67cec9db07
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2022-03-03 10:25:22 -0800

    Update evacuee-profile.service.ts

--- responders/src/UI/embc-responder/src/app/core/services/evacuee-profile.service.ts
+++ responders/src/UI/embc-responder/src/app/core/services/evacuee-profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, mergeMap, withLatestFrom } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 import { RegistrationsService } from 'src/app/core/api/services';
 import {
   EvacuationFileSearchResult,
@@ -167,11 +167,12 @@ export class EvacueeProfileService {
     regProfile: RegistrantProfile,
     memberId: string,
     essFileId: string
-  ): Observable<void> {
+  ): Observable<Blob> {
     const profile$ =
       this.registrationsService.registrationsCreateRegistrantProfile({
         body: regProfile
       });
+
     const $result = profile$.pipe(
       mergeMap((regResult) =>
         this.linkMemberProfile({
@@ -181,11 +182,11 @@ export class EvacueeProfileService {
             registantId: regResult.id
           }
         })
-      ),
-      withLatestFrom(profile$),
-      map(([blob, regResult]) => {
-        this.evacueeSessionService.profileId = regResult.id;
-      })
+      )
+      // withLatestFrom(profile$),
+      // map(([blob, regResult]) => {
+      //   this.evacueeSessionService.profileId = regResult.id;
+      // })
     );
     return $result;
   }
