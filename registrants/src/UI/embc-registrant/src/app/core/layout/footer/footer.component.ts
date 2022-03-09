49ab03bf357c805cd8bb68ecf99c609370dc6717
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-03-08 18:36:40 -0800

    fix(): lint fixes

--- registrants/src/UI/embc-registrant/src/app/core/layout/footer/footer.component.ts
+++ registrants/src/UI/embc-registrant/src/app/core/layout/footer/footer.component.ts
@@ -11,39 +11,37 @@ import { DialogContent } from '../../model/dialog-content.model';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  constructor(private httpClient: HttpClient, private dialog: MatDialog) {}
 
-  constructor(
-    private httpClient: HttpClient,
-    private dialog: MatDialog) { }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   openVersionsModal(): void {
-    this.httpClient.get('version')
-      .subscribe(response => {
-        const content = response as { name: string, version: string }[];
-        const dialog = this.dialog.open(DialogComponent, {
-          data: {
-            component: InformationDialogComponent,
-            content: this.versionDialog(content)
-          },
-          height: '345px',
-          width: '530px'
-        });
+    this.httpClient.get('version').subscribe((response) => {
+      const content = response as { name: string; version: string }[];
+      const dialog = this.dialog.open(DialogComponent, {
+        data: {
+          component: InformationDialogComponent,
+          content: this.versionDialog(content)
+        },
+        height: '345px',
+        width: '530px'
       });
+    });
   }
 
-  private versionDialog(versions: { name: string, version: string }[]): DialogContent {
-    const rows = versions.map(version => `<tr><td>${version.name}</td><td>${version.version}</td></tr>`).join('');
-    console.debug(rows);
+  private versionDialog(
+    versions: { name: string; version: string }[]
+  ): DialogContent {
+    const rows = versions
+      .map(
+        (version) =>
+          `<tr><td>${version.name}</td><td>${version.version}</td></tr>`
+      )
+      .join('');
     return {
       text: `<table class="versions-table">${rows}</table>`,
       cancelButton: 'Close',
-      title: "Version Information"
+      title: 'Version Information'
     };
-
   }
-
 }
--- responders/src/UI/embc-responder/src/app/core/layout/footer/footer.component.ts
+++ responders/src/UI/embc-responder/src/app/core/layout/footer/footer.component.ts
@@ -14,37 +14,39 @@ import { DialogContent } from '../../models/dialog-content.model';
 export class FooterComponent implements OnInit {
   @Input() appVersion: Array<VersionInformation>;
 
-  constructor(
-    private httpClient: HttpClient,
-    private dialog: MatDialog) {}
+  constructor(private httpClient: HttpClient, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     // this.appVersion = environment.version;
   }
 
   openVersionsModal(): void {
-    this.httpClient.get('version')
-      .subscribe(response => {
-        const content = response as { name: string, version: string }[];
-        const dialog = this.dialog.open(DialogComponent, {
-          data: {
-            component: InformationDialogComponent,
-            content: this.versionDialog(content)
-          },
-          height: '345px',
-          width: '530px'
-        });
+    this.httpClient.get('version').subscribe((response) => {
+      const content = response as { name: string; version: string }[];
+      const dialog = this.dialog.open(DialogComponent, {
+        data: {
+          component: InformationDialogComponent,
+          content: this.versionDialog(content)
+        },
+        height: '345px',
+        width: '530px'
       });
+    });
   }
 
-  private versionDialog(versions: { name: string, version: string }[]): DialogContent {
-    const rows = versions.map(version => `<tr><td>${version.name}</td><td>${version.version}</td></tr>`).join('');
-    console.debug(rows);
+  private versionDialog(
+    versions: { name: string; version: string }[]
+  ): DialogContent {
+    const rows = versions
+      .map(
+        (version) =>
+          `<tr><td>${version.name}</td><td>${version.version}</td></tr>`
+      )
+      .join('');
     return {
       text: `<table class="versions-table">${rows}</table>`,
       cancelButton: 'Close',
-      title: "Version Information"
+      title: 'Version Information'
     };
-
   }
 }
