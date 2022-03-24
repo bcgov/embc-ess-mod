233df75040a4cfa05d7560bb3bc8c3544e002edc
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2022-03-24 14:12:04 -0700

    Package.json and API models update

--- registrants/src/UI/embc-registrant/src/app/core/api/api-configuration.ts
+++ registrants/src/UI/embc-registrant/src/app/core/api/api-configuration.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:5020';
+  rootUrl: string = 'https://dev1-era-registrants.apps.silver.devops.gov.bc.ca';
 }
 
 /**
