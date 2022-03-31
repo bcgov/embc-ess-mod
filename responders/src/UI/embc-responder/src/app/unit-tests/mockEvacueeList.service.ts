11a320d1a108c59df635397f717145c92aa2bc47
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2022-03-31 15:44:42 -0700

    EMBCESSMOD-3644

--- responders/src/UI/embc-responder/src/app/unit-tests/mockEvacueeList.service.ts
+++ responders/src/UI/embc-responder/src/app/unit-tests/mockEvacueeList.service.ts
@@ -1,9 +1,195 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { Code } from '../core/api/models';
 import { LoadEvacueeListService } from '../core/services/load-evacuee-list.service';
 
 @Injectable({ providedIn: 'root' })
 export class MockEvacueeListService extends LoadEvacueeListService {
+  public categoryList: Code[] = [
+    {
+      type: 'SupportCategory',
+      value: 'Unknown',
+      description: '',
+      parentCode: null
+    },
+    {
+      type: 'SupportCategory',
+      value: 'Clothing',
+      description: 'Clothing',
+      parentCode: null
+    },
+    {
+      type: 'SupportCategory',
+      value: 'Food',
+      description: 'Food',
+      parentCode: null
+    },
+    {
+      type: 'SupportCategory',
+      value: 'Incidentals',
+      description: 'Incidentals',
+      parentCode: null
+    },
+    {
+      type: 'SupportCategory',
+      value: 'Lodging',
+      description: 'Lodging',
+      parentCode: null
+    },
+    {
+      type: 'SupportCategory',
+      value: 'Transportation',
+      description: 'Transportation',
+      parentCode: null
+    }
+  ];
+
+  public subCategoryList: Code[] = [
+    {
+      type: 'SupportSubCategory',
+      value: 'None',
+      description: '',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Lodging_Hotel',
+      description: 'Lodging - Hotel/Motel',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Lodging_Billeting',
+      description: 'Lodging - Billeting',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Lodging_Group',
+      description: 'Lodging - Group Lodging',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Food_Groceries',
+      description: 'Food - Groceries',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Food_Restaurant',
+      description: 'Food - Restaurant Meals',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Transportation_Taxi',
+      description: 'Transportation - Taxi',
+      parentCode: null
+    },
+    {
+      type: 'SupportSubCategory',
+      value: 'Transportation_Other',
+      description: 'Transportation - Other',
+      parentCode: null
+    }
+  ];
+
+  public supportStatus: Code[] = [
+    {
+      type: 'SupportStatus',
+      value: 'Draft',
+      description: 'Draft',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Active',
+      description: 'Active',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Expired',
+      description: 'Expired',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Void',
+      description: 'Void',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'PendingApproval',
+      description: 'Pending Approval',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'UnderReview',
+      description: 'Pending Approval',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Approved',
+      description: 'Approved',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Paid',
+      description: 'Paid',
+      parentCode: null
+    },
+    {
+      type: 'SupportStatus',
+      value: 'Cancelled',
+      description: 'Cancelled',
+      parentCode: null
+    }
+  ];
+
+  public supportMethods: Code[] = [
+    {
+      type: 'SupportMethod',
+      value: 'Unknown',
+      description: '',
+      parentCode: null
+    },
+    {
+      type: 'SupportMethod',
+      value: 'Referral',
+      description: 'Referral',
+      parentCode: null
+    },
+    {
+      type: 'SupportMethod',
+      value: 'ETransfer',
+      description: 'e-Transfer',
+      parentCode: null
+    }
+  ];
+
   public loadStaticEvacueeLists(): Promise<void> {
     return Promise.resolve();
+  }
+
+  public getSupportCategories(): Code[] {
+    return this.categoryList;
+  }
+
+  public getSupportSubCategories(): Code[] {
+    return this.subCategoryList;
+  }
+
+  public getSupportStatus(): Code[] {
+    return this.supportStatus;
+  }
+
+  public getSupportMethods(): Code[] {
+    return this.supportMethods;
   }
 }
--- responders/src/UI/embc-responder/src/app/unit-tests/mockStepEssFile.service.ts
+++ responders/src/UI/embc-responder/src/app/unit-tests/mockStepEssFile.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@angular/core';
+import {
+  CommunityType,
+  EvacuationFileStatus,
+  HouseholdMemberType,
+  InsuranceOption,
+  NeedsAssessmentType,
+  SupportCategory,
+  SupportMethod,
+  SupportStatus,
+  SupportSubCategory
+} from '../core/api/models';
+import { EvacuationFileModel } from '../core/models/evacuation-file.model';
 import { TabModel } from '../core/models/tab.model';
 import { StepEssFileService } from '../feature-components/wizard/step-ess-file/step-ess-file.service';
 
@@ -7,6 +19,15 @@ import { StepEssFileService } from '../feature-components/wizard/step-ess-file/s
 })
 export class MockStepEssFileService extends StepEssFileService {
   public essTabsValue: Array<TabModel>;
+  public essFileValue: EvacuationFileModel;
+
+  public get selectedEssFile(): EvacuationFileModel {
+    return this.essFileValue;
+  }
+
+  public set selectedEssFile(essFile: EvacuationFileModel) {
+    this.essFileValue = essFile;
+  }
 
   public get essTabs(): Array<TabModel> {
     return this.essTabsValue;
@@ -104,4 +125,151 @@ export class MockStepEssFileService extends StepEssFileService {
       previous: '/ess-wizard/ess-file/needs'
     }
   ];
+
+  public essFile: EvacuationFileModel = {
+    id: '154150',
+    externalReferenceId: null,
+    completedOn: null,
+    completedBy: null,
+    isPaper: false,
+    primaryRegistrantId: 'a7b76c4b-256b-4385-b35e-7b496e70f172',
+    primaryRegistrantFirstName: 'julia',
+    primaryRegistrantLastName: 'roberts',
+    evacuatedFromAddress: {
+      community: {
+        code: '9069dfaf-9f97-ea11-b813-005056830319',
+        name: 'Chetwynd',
+        districtName: 'Peace River',
+        stateProvinceCode: 'BC',
+        countryCode: 'CAN',
+        type: CommunityType.DistrictMunicipality
+      },
+      addressLine1: '890 Dolphin St',
+      addressLine2: null,
+      city: null,
+      communityCode: '9069dfaf-9f97-ea11-b813-005056830319',
+      stateProvinceCode: null,
+      countryCode: null,
+      postalCode: null,
+      country: { name: 'Canada', code: 'CAN' }
+    },
+    registrationLocation: 'location',
+    needsAssessment: {
+      id: '36e9fedc-ffcf-4aa9-a177-10f211ecfc33',
+      createdOn: '2022-03-28T23:52:31Z',
+      modifiedOn: '2022-03-28T23:52:31Z',
+      reviewingTeamMemberId: '49475fd6-df73-ec11-b830-00505683fbf4',
+      reviewingTeamMemberDisplayName: 'Sue T.',
+      insurance: InsuranceOption.Unsure,
+      evacuationImpact: 'evac',
+      evacuationExternalReferrals: null,
+      petCarePlans: null,
+      houseHoldRecoveryPlan: null,
+      recommendedReferralServices: [],
+      householdMembers: [
+        {
+          id: '76d303d5-7466-47ad-9df8-0c117fa56980',
+          linkedRegistrantId: 'a7b76c4b-256b-4385-b35e-7b496e70f172',
+          firstName: 'julia',
+          lastName: 'roberts',
+          initials: null,
+          gender: 'Female',
+          dateOfBirth: '12/12/2000',
+          type: HouseholdMemberType.Registrant,
+          isPrimaryRegistrant: true,
+          isHouseholdMember: false,
+          isMinor: false,
+          isRestricted: false,
+          isVerified: true
+        }
+      ],
+      haveSpecialDiet: false,
+      specialDietDetails: null,
+      takeMedication: false,
+      haveMedicalSupplies: false,
+      pets: [],
+      havePetsFood: false,
+      canProvideFood: true,
+      canProvideLodging: true,
+      canProvideClothing: true,
+      canProvideTransportation: true,
+      canProvideIncidentals: true,
+      type: NeedsAssessmentType.Assessed
+    },
+    notes: [],
+    supports: [
+      {
+        category: SupportCategory.Food,
+        subCategory: SupportSubCategory.Food_Groceries,
+        id: 'D2035834',
+        fileId: '154150',
+        createdOn: '2022-03-28T23:53:59Z',
+        createdBy: 'Sue, T',
+        createdByTeam: 'DEV Team',
+        issuedOn: '2022-03-28T23:53:58Z',
+        issuedBy: 'Sue, T',
+        issuedByTeam: 'DEV Team',
+        needsAssessmentId: '36e9fedc-ffcf-4aa9-a177-10f211ecfc33',
+        from: '2022-03-28T23:53:00Z',
+        to: '2022-03-29T23:54:00Z',
+        status: SupportStatus.Active,
+        method: SupportMethod.Referral,
+        supportDelivery: {
+          method: SupportMethod.Referral
+        },
+        includedHouseholdMembers: ['76d303d5-7466-47ad-9df8-0c117fa56980']
+      }
+    ],
+    securityPhrase: 's****e',
+    securityPhraseEdited: false,
+    isRestricted: false,
+    status: EvacuationFileStatus.Active,
+    evacuationFileDate: '2022-03-28T23:52:28Z',
+    householdMembers: [
+      {
+        id: '76d303d5-7466-47ad-9df8-0c117fa56980',
+        linkedRegistrantId: 'a7b76c4b-256b-4385-b35e-7b496e70f172',
+        firstName: 'julia',
+        lastName: 'roberts',
+        initials: null,
+        gender: 'Female',
+        dateOfBirth: '12/12/2000',
+        type: HouseholdMemberType.Registrant,
+        isPrimaryRegistrant: true,
+        isHouseholdMember: false,
+        isMinor: false,
+        isRestricted: false,
+        isVerified: true
+      }
+    ],
+    task: {
+      taskNumber: 'test',
+      communityCode: '7269dfaf-9f97-ea11-b813-005056830319',
+      from: '2021-03-23T17:26:00Z',
+      to: '2022-04-14T15:00:00Z',
+      status: 'Active',
+      features: [
+        {
+          name: 'digital-support-referrals',
+          enabled: true
+        },
+        {
+          name: 'digital-support-etransfer',
+          enabled: true
+        },
+        {
+          name: 'paper-support-referrals',
+          enabled: false
+        }
+      ]
+    },
+    assignedTaskCommunity: {
+      code: '7269dfaf-9f97-ea11-b813-005056830319',
+      name: 'Alert Bay',
+      districtName: 'Mount Waddington',
+      stateProvinceCode: 'BC',
+      countryCode: 'CAN',
+      type: CommunityType.Village
+    }
+  };
 }
