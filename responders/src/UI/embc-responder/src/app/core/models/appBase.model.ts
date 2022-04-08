3431dfb2c5bd6931e9017da2702fd6f08b9fb480
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2022-04-08 13:59:53 -0700

    Update appBase.model.ts

--- responders/src/UI/embc-responder/src/app/core/models/appBase.model.ts
+++ responders/src/UI/embc-responder/src/app/core/models/appBase.model.ts
@@ -1,4 +1,6 @@
+import { Code } from '../api/models';
 import { EssTaskModel } from './ess-task.model';
+import { EvacuationFileModel } from './evacuation-file.model';
 import { RegistrantProfileModel } from './registrant-profile.model';
 
 export interface AppBaseModel {
@@ -6,6 +8,9 @@ export interface AppBaseModel {
   selectedUserPathway?: SelectedPathType;
   evacueeSearchType?: DigitalFlow | PaperFlow;
   selectedProfile?: SelectedProfile;
+  etransferProperties?: EtransferProperties;
+  selectedEssFile?: EvacuationFileModel;
+  supportProperties?: SupportProperties;
 }
 
 export interface EvacueeSearch {
@@ -18,7 +23,17 @@ export type PaperFlow = EvacueeSearch;
 
 export interface SelectedProfile {
   selectedEvacueeInContext?: RegistrantProfileModel;
-  features?: [];
+}
+
+export interface EtransferProperties {
+  isRegistrantEtransferEligible?: boolean;
+  etransferStatus?: ETransferStatus;
+  interacAllowed?: boolean; //will come from backend
+  etransferRequirement?: Array<EtransferRequirementStatus>;
+}
+
+export interface SupportProperties {
+  selectedSupport?: Code;
 }
 
 export enum SelectedPathType {
