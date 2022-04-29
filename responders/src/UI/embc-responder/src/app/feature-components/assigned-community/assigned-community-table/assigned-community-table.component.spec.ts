873dea7a4295a9a90e674f3309bb6f824b9ac459
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2022-04-29 12:49:52 -0700

    fix ng test

--- responders/src/UI/embc-responder/src/app/feature-components/assigned-community/assigned-community-table/assigned-community-table.component.spec.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/assigned-community/assigned-community-table/assigned-community-table.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AssignedCommunityTableComponent } from './assigned-community-table.component';
@@ -8,6 +9,7 @@ describe('DataTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [AssignedCommunityTableComponent]
     }).compileComponents();
   });
