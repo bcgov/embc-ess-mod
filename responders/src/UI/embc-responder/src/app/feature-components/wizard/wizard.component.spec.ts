fa26bf8fda68a6d28c46a6a635ee892bd836a99e
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2022-05-05 13:52:42 -0700

    test fix

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/wizard.component.spec.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/wizard.component.spec.ts
@@ -18,7 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StepEssFileComponent } from './step-ess-file/step-ess-file.component';
 import { By } from '@angular/platform-browser';
 import { computeInterfaceToken } from 'src/app/app.module';
-import { StepSupportsService } from './step-supports/step-supports.service';
+import { DatePipe } from '@angular/common';
 
 describe('WizardComponent-Test for New Registration', () => {
   let component: WizardComponent;
@@ -51,7 +51,7 @@ describe('WizardComponent-Test for New Registration', () => {
         providers: [
           WizardComponent,
           WizardDataService,
-          StepSupportsService,
+          DatePipe,
           { provide: ActivatedRoute, useValue: activatedRouteMock },
           {
             provide: WizardService,
