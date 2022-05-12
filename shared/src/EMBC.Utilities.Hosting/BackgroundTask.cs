dd810d228ab01e4a5cc638cba6e812ad74d8e0e1
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-05-12 11:43:52 -0700

    refactor(backgroundtasks): replaced ncrontab with cronos for better future support

--- shared/src/EMBC.Utilities.Hosting/BackgroundTask.cs
+++ shared/src/EMBC.Utilities.Hosting/BackgroundTask.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Cronos;
 using EMBC.Utilities.Caching;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NCrontab;
 
 namespace EMBC.Utilities.Hosting
 {
@@ -18,7 +18,7 @@ namespace EMBC.Utilities.Hosting
     {
         private readonly IServiceProvider serviceProvider;
         private readonly ILogger<T> logger;
-        private readonly CrontabSchedule schedule;
+        private readonly CronExpression schedule;
         private readonly TimeSpan startupDelay;
         private readonly BackgroundTaskConcurrencyManager concurrencyManager;
         private string instanceName => Environment.MachineName;
@@ -33,7 +33,7 @@ namespace EMBC.Utilities.Hosting
                 var configuration = serviceProvider.GetRequiredService<IConfiguration>().GetSection($"backgroundtask:{typeof(T).Name}");
                 var initialTask = scope.ServiceProvider.GetRequiredService<T>();
 
-                schedule = CrontabSchedule.Parse(configuration.GetValue("schedule", initialTask.Schedule), new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+                schedule = CronExpression.Parse(configuration.GetValue("schedule", initialTask.Schedule), CronFormat.IncludeSeconds);
                 startupDelay = configuration.GetValue("initialDelay", initialTask.InitialDelay);
                 enabled = configuration.GetValue("enabled", true);
 
@@ -54,7 +54,7 @@ namespace EMBC.Utilities.Hosting
             }
             await Task.Delay(startupDelay, stoppingToken);
             var now = DateTime.UtcNow;
-            var nextExecutionDate = schedule.GetNextOccurrence(now);
+            var nextExecutionDate = CalculateNextExecutionDate(now);
 
             logger.LogDebug("first run is {0} in {1}s", nextExecutionDate, nextExecutionDate.Subtract(now).TotalSeconds);
 
@@ -85,7 +85,7 @@ namespace EMBC.Utilities.Hosting
                     }
                     finally
                     {
-                        nextExecutionDate = schedule.GetNextOccurrence(now);
+                        nextExecutionDate = CalculateNextExecutionDate(now);
                         logger.LogDebug("next run is {0} in {1}s", nextExecutionDate, nextExecutionDate.Subtract(DateTime.UtcNow).TotalSeconds);
                     }
                 }
@@ -94,18 +94,26 @@ namespace EMBC.Utilities.Hosting
             }
         }
 
+        private DateTime CalculateNextExecutionDate(DateTime utcNow)
+        {
+            var nextDate = schedule.GetNextOccurrence(utcNow);
+            if (nextDate == null) throw new InvalidOperationException("Cannot calculate the next execution date, stopping the background task");
+
+            return nextDate.Value;
+        }
+
         public override async Task StartAsync(CancellationToken cancellationToken)
         {
             logger.LogInformation("starting");
             await base.StartAsync(cancellationToken);
         }
 
-        public override async Task StopAsync(CancellationToken stoppingToken)
+        public override async Task StopAsync(CancellationToken cancellationToken)
         {
-            logger.LogInformation("deregistrering instance");
-            await concurrencyManager.Deregister(instanceName, stoppingToken);
+            logger.LogDebug("unregistering instance");
+            await concurrencyManager.Deregister(instanceName, cancellationToken);
             logger.LogInformation("stopping");
-            await base.StopAsync(stoppingToken);
+            await base.StopAsync(cancellationToken);
         }
     }
 
@@ -172,16 +180,16 @@ namespace EMBC.Utilities.Hosting
                 await cache.Set(cacheKey, state, TimeSpan.FromMinutes(30), cancellationToken);
             }
         }
-    }
 
-    public class ConcurrencyState : Dictionary<string, DateTime>
-    {
-        public void Trim(TimeSpan timeout)
+        public class ConcurrencyState : Dictionary<string, DateTime>
         {
-            var now = DateTime.UtcNow;
-            foreach (var s in this.Where(s => now.Subtract(s.Value) > timeout).ToArray())
+            public void Trim(TimeSpan timeout)
             {
-                Remove(s.Key);
+                var now = DateTime.UtcNow;
+                foreach (var s in this.Where(s => now.Subtract(s.Value) > timeout).ToArray())
+                {
+                    Remove(s.Key);
+                }
             }
         }
     }
