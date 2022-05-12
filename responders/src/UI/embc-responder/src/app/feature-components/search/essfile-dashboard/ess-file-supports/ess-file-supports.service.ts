4fa9c63027efa2f12fd3ceecfd982b1ba5b94229
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2022-05-12 16:06:51 -0700

    EMBCESSMOD-3848

--- responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/ess-file-supports/ess-file-supports.service.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/ess-file-supports/ess-file-supports.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { SupportStatus } from 'src/app/core/api/models/support-status';
+import { SupportStatus } from 'src/app/core/api/models';
 import {
   ObjectWrapper,
   TableFilterModel
 } from 'src/app/core/models/table-filter.model';
 import { LoadEvacueeListService } from 'src/app/core/services/load-evacuee-list.service';
-import { EvacueeSearchService } from '../../evacuee-search/evacuee-search.service';
 
 @Injectable({ providedIn: 'root' })
 export class EssFileSupportsService {
@@ -19,10 +18,7 @@ export class EssFileSupportsService {
     description: 'All Status'
   };
 
-  constructor(
-    private evacueeSearchService: EvacueeSearchService,
-    private loadEvacueeListService: LoadEvacueeListService
-  ) {}
+  constructor(private loadEvacueeListService: LoadEvacueeListService) {}
 
   public load(): TableFilterModel {
     return {
@@ -42,6 +38,7 @@ export class EssFileSupportsService {
             .filter(
               (support, index, self) =>
                 support.description &&
+                support.description !== SupportStatus.Draft &&
                 self.findIndex((s) => s.description === support.description) ===
                   index
             )
