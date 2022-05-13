545580f5689600f92d0af8aa75b5f57b7d51925d
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-05-13 14:18:48 -0700

    refactor(oauth-server): logging and telemetry

--- oauth-server/src/OAuthServer/Program.cs
+++ oauth-server/src/OAuthServer/Program.cs
@@ -1,22 +1,26 @@
 ﻿using System;
-using System.Net.Http;
+using System.Runtime;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 
 namespace OAuthServer
 {
-    public class Program
+    public static class Program
     {
+        private static string appName => Environment.GetEnvironmentVariable("APP_NAME") ?? "oauth-server";
+
         public static int Main(string[] args)
         {
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateBootstrapLogger();
+               .MinimumLevel.Debug()
+               .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+               .Enrich.FromLogContext()
+               .WriteTo.Console(outputTemplate: Logging.LogOutputTemplate)
+               .CreateBootstrapLogger();
 
             try
             {
@@ -37,36 +41,7 @@ namespace OAuthServer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((hostingContext, loggerConfiguration) =>
-                {
-                    loggerConfiguration
-                        .ReadFrom.Configuration(hostingContext.Configuration)
-                        .Enrich.FromLogContext()
-                        .WriteTo.Console();
-
-                    if (!hostingContext.HostingEnvironment.IsDevelopment())
-                    {
-                        var splunkUrl = hostingContext.Configuration.GetValue("SPLUNK_URL", string.Empty);
-                        var splunkToken = hostingContext.Configuration.GetValue("SPLUNK_TOKEN", string.Empty);
-                        if (string.IsNullOrWhiteSpace(splunkToken) || string.IsNullOrWhiteSpace(splunkUrl))
-                        {
-                            Log.Warning($"Logs will NOT be forwarded to Splunk: check SPLUNK_TOKEN and SPLUNK_URL env vars");
-                        }
-                        else
-                        {
-                            loggerConfiguration
-                                .WriteTo.EventCollector(
-                                    splunkHost: splunkUrl,
-                                    eventCollectorToken: splunkToken,
-                                    messageHandler: new HttpClientHandler
-                                    {
-                                        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                                    },
-                                    renderTemplate: false);
-                            Log.Warning($"Logs will be forwarded to Splunk");
-                        }
-                    }
-                })
+                .UseSerilog((ctx, config) => Logging.ConfigureSerilog(ctx, config, appName))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
