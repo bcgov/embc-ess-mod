198d2ae64f2d1fadb84902af05c8ab13eacd2326
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2022-05-20 09:30:28 -0700

    EMBCESSMOD-3949

--- automated-tests/src/EMBC.Tests.Automation/EMBC.Tests.Automation.Registrants/Features/AuthenticatedRegistrant.feature
+++ automated-tests/src/EMBC.Tests.Automation/EMBC.Tests.Automation.Registrants/Features/AuthenticatedRegistrant.feature
@@ -3,3 +3,9 @@
 Scenario: BCSC user logs in to dashboard	
 	Given I log in with BCSC credentials EVAC00006
 	Then I am on path /verified-registration/dashboard/current
+
+Scenario: Create new ESS File
+	Given I log in with BCSC credentials EVAC00006
+	When I create a new EssFile
+	And I complete the maximum fields on the ESS file evacuee forms
+	Then the ESS File submission complete dialog appears
---
+++ automated-tests/src/EMBC.Tests.Automation/EMBC.Tests.Automation.Registrants/PageObjects/EvacueeDashboard.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMBC.Tests.Automation.Registrants.PageObjects
+{
+    public class EvacueeDashboard : PageObjectBase
+    {
+
+        public EvacueeDashboard(IWebDriver webDriver) : base(webDriver)
+        { }
+
+        public void CreateNewEvent()
+        {
+            ButtonElement("Add Another Event");
+            ButtonElement("Yes, Continue");
+        }
+    }
+}
