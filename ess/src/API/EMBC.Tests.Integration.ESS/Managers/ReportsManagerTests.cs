3a8f378a7f05ce0c695bb2d40421a16c9d5c3c03
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-08-17 08:16:14 -0700

    fix(reports): minor fixes

--- ess/src/API/EMBC.Tests.Integration.ESS/Managers/ReportsManagerTests.cs
+++ ess/src/API/EMBC.Tests.Integration.ESS/Managers/ReportsManagerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using EMBC.ESS.Managers.Reports;
 using EMBC.ESS.Shared.Contracts.Reports;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,11 +8,11 @@ using Xunit.Abstractions;
 
 namespace EMBC.Tests.Integration.ESS.Managers
 {
-    public class ReportsManagerTests : DynamicsWebAppTestBase
+    public class ReportsManagerTests : WebAppTestBase
     {
         private readonly ReportsManager reportsManager;
 
-        public ReportsManagerTests(ITestOutputHelper output, DynamicsWebAppFixture fixture) : base(output, fixture)
+        public ReportsManagerTests(ITestOutputHelper output, WebAppTestFixture fixture) : base(output, fixture)
         {
             reportsManager = Services.GetRequiredService<ReportsManager>();
         }
@@ -23,13 +22,13 @@ namespace EMBC.Tests.Integration.ESS.Managers
         {
             var res = await reportsManager.Handle(new EvacueeReportQuery
             {
-                TaskNumber = TestData.ActiveTaskId,
+                TaskNumber = "1234",
                 //FileId = "101010",
                 //EvacuatedFrom = "9e6adfaf-9f97-ea11-b813-005056830319",
                 //EvacuatedTo = "9e6adfaf-9f97-ea11-b813-005056830319",
                 IncludePersonalInfo = false,
-                From = DateTime.UtcNow.AddDays(-1),
-                To = DateTime.UtcNow,
+                //From = DateTime.UtcNow.AddDays(-1),
+                //To = DateTime.UtcNow,
             });
 
             res.Content.ShouldNotBeEmpty();
@@ -41,7 +40,7 @@ namespace EMBC.Tests.Integration.ESS.Managers
         {
             var res = await reportsManager.Handle(new SupportReportQuery
             {
-                TaskNumber = TestData.ActiveTaskId,
+                TaskNumber = "1234",
                 //FileId = "101010",
                 //EvacuatedFrom = "9e6adfaf-9f97-ea11-b813-005056830319",
                 //EvacuatedTo = "9e6adfaf-9f97-ea11-b813-005056830319",
