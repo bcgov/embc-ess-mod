f71a410164c06093ae861fc356d97a9ba7890a87
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2022-08-22 12:29:52 -0700

    Update essfile-dashboard.component.ts

--- responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/essfile-dashboard.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/search/essfile-dashboard/essfile-dashboard.component.ts
@@ -1,30 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { AddressModel } from 'src/app/core/models/address.model';
 import { EvacuationFileModel } from 'src/app/core/models/evacuation-file.model';
-import { WizardType } from 'src/app/core/models/wizard-type.model';
 import { EssFileService } from 'src/app/core/services/ess-file.service';
-import { EvacueeSessionService } from 'src/app/core/services/evacuee-session.service';
 import { Community } from 'src/app/core/services/locations.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
-import { FileStatusDefinitionComponent } from 'src/app/shared/components/dialog-components/file-status-definition/file-status-definition.component';
-import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 import { EssfileDashboardService } from './essfile-dashboard.service';
 import * as globalConst from '../../../core/services/global-constants';
-import { EvacuationFileStatus, Note } from 'src/app/core/api/models';
+import { Note } from 'src/app/core/api/models';
 import { map } from 'rxjs/operators';
-import { InformationDialogComponent } from 'src/app/shared/components/dialog-components/information-dialog/information-dialog.component';
-import {
-  DashboardBanner,
-  DialogContent
-} from 'src/app/core/models/dialog-content.model';
+import { DashboardBanner } from 'src/app/core/models/dialog-content.model';
 import { AppBaseService } from 'src/app/core/services/helper/appBase.service';
-import { ComputeRulesService } from 'src/app/core/services/computeRules.service';
 import { EvacueeSearchService } from '../evacuee-search/evacuee-search.service';
-import { RegistrantProfileModel } from 'src/app/core/models/registrant-profile.model';
-import { EvacueeProfileService } from 'src/app/core/services/evacuee-profile.service';
-import { lastValueFrom, tap } from 'rxjs';
 import { OptionInjectionService } from 'src/app/core/interfaces/searchOptions.service';
 import { SelectedPathType } from 'src/app/core/models/appBase.model';
 
@@ -47,15 +34,11 @@ export class EssfileDashboardComponent implements OnInit {
 
   constructor(
     private essFileService: EssFileService,
-    public evacueeSessionService: EvacueeSessionService,
-    private dialog: MatDialog,
     private router: Router,
     private essfileDashboardService: EssfileDashboardService,
     private alertService: AlertService,
     public appBaseService: AppBaseService,
-    private computeState: ComputeRulesService,
     private evacueeSearchService: EvacueeSearchService,
-    private evacueeProfileService: EvacueeProfileService,
     private optionInjectionService: OptionInjectionService
   ) {}
 
@@ -87,35 +70,15 @@ export class EssfileDashboardComponent implements OnInit {
         });
     }
 
-    const profile$ = await this.updateMember();
+    const profile$ = await this.essfileDashboardService.updateMember();
 
     this.isMinor =
       this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext?.isMinor;
     this.isLinkedToBcsc =
       this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext?.authenticatedUser;
-    this.hasPostal = this.hasPostalCode();
+    this.hasPostal = this.essfileDashboardService.hasPostalCode();
 
-    if (this.evacueeSessionService.fileLinkStatus === 'S') {
-      this.openLinkDialog(globalConst.profileLinkMessage)
-        .afterClosed()
-        .subscribe({
-          next: (value) => {
-            this.evacueeSessionService.fileLinkFlag = null;
-            this.evacueeSessionService.fileLinkMetaData = null;
-            this.evacueeSessionService.fileLinkStatus = null;
-          }
-        });
-    } else if (this.evacueeSessionService.fileLinkStatus === 'E') {
-      this.openLinkDialog(globalConst.profileLinkErrorMessage)
-        .afterClosed()
-        .subscribe({
-          next: (value) => {
-            this.evacueeSessionService.fileLinkFlag = null;
-            this.evacueeSessionService.fileLinkMetaData = null;
-            this.evacueeSessionService.fileLinkStatus = null;
-          }
-        });
-    }
+    this.essfileDashboardService.showFileLinkingPopups();
 
     this.eligiblityDisplayName();
   }
@@ -135,108 +98,23 @@ export class EssfileDashboardComponent implements OnInit {
    * profile status
    */
   openStatusDefinition(): void {
-    this.dialog.open(DialogComponent, {
-      data: {
-        component: FileStatusDefinitionComponent,
-        content: this.essFile.status
-      },
-      width: '580px'
-    });
-  }
-
-  /**
-   * Navigates the wizard for review ess file
-   */
-  reviewEssFile(): void {
-    this.isLoading = !this.isLoading;
-
-    this.appBaseService.wizardProperties = {
-      wizardType: WizardType.ReviewFile,
-      lastCompletedStep: null,
-      editFlag: true,
-      memberFlag: false
-    };
-    this.computeState.triggerEvent();
-
-    this.router
-      .navigate(['/ess-wizard'], {
-        queryParams: { type: WizardType.ReviewFile },
-        queryParamsHandling: 'merge'
-      })
-      .then(() => {
-        this.isLoading = !this.isLoading;
-      })
-      .catch(() => {
-        this.isLoading = !this.isLoading;
-      });
-  }
-
-  /**
-   * Navigates the wizard for complete ess file
-   */
-  completeEssFile(): void {
-    this.isLoading = !this.isLoading;
-
-    this.appBaseService.wizardProperties = {
-      wizardType: WizardType.CompleteFile,
-      lastCompletedStep: null,
-      editFlag: true,
-      memberFlag: false
-    };
-    this.computeState.triggerEvent();
-
-    this.router
-      .navigate(['/ess-wizard'], {
-        queryParams: { type: WizardType.CompleteFile },
-        queryParamsHandling: 'merge'
-      })
-      .then(() => {
-        this.isLoading = !this.isLoading;
-      })
-      .catch(() => {
-        this.isLoading = !this.isLoading;
-      });
-  }
-
-  extendSupports(): void {
-    this.isLoading = !this.isLoading;
-
-    this.appBaseService.wizardProperties = {
-      wizardType: WizardType.ExtendSupports,
-      lastCompletedStep: null,
-      editFlag: false,
-      memberFlag: false
-    };
-    this.computeState.triggerEvent();
-
-    this.router
-      .navigate(['/ess-wizard'], {
-        queryParams: { type: WizardType.ExtendSupports },
-        queryParamsHandling: 'merge'
-      })
-      .then(() => {
-        this.isLoading = !this.isLoading;
-      })
-      .catch(() => {
-        this.isLoading = !this.isLoading;
-      });
+    this.essfileDashboardService.openStatusDefinition();
   }
 
   openWizard() {
-    if (
-      this.essFile.status === EvacuationFileStatus.Pending ||
-      this.essFile.status === EvacuationFileStatus.Expired
-    ) {
-      this.completeEssFile();
-    } else if (
-      this.essFile.status === EvacuationFileStatus.Active &&
-      this.optionInjectionService.instance.optionType ===
-        SelectedPathType.remoteExtensions
-    ) {
-      this.extendSupports();
-    } else {
-      this.reviewEssFile();
-    }
+    const wizardType = this.essfileDashboardService.getWizardType(
+      this.optionInjectionService.instance.optionType
+    );
+    this.isLoading = !this.isLoading;
+
+    this.optionInjectionService.instance
+      .openWizard(wizardType)
+      .then((value) => {
+        this.isLoading = !this.isLoading;
+      })
+      .catch(() => {
+        this.isLoading = !this.isLoading;
+      });
   }
 
   /**
@@ -303,74 +181,5 @@ export class EssfileDashboardComponent implements OnInit {
       this.eligibilityLastName =
         this.evacueeSearchService?.evacueeSearchContext?.evacueeSearchParameters?.lastName;
     }
-  }
-
-  /**
-   * Opens link success dialog box
-   *
-   * @returns mat dialog reference
-   */
-  private openLinkDialog(displayMessage: DialogContent) {
-    return this.dialog.open(DialogComponent, {
-      data: {
-        component: InformationDialogComponent,
-        content: displayMessage
-      },
-      width: '530px'
-    });
-  }
-
-  private async updateMember() {
-    if (
-      this.appBaseService?.appModel?.selectedProfile
-        ?.householdMemberRegistrantId !== undefined
-    ) {
-      if (this.appBaseService?.appModel?.selectedProfile.profileReloadFlag) {
-        const profile$ = await this.getEvacueeProfile(
-          this.appBaseService?.appModel?.selectedProfile
-            ?.selectedEvacueeInContext?.id
-        );
-      }
-      this.appBaseService.appModel = {
-        selectedProfile: {
-          selectedEvacueeInContext:
-            this.appBaseService?.appModel?.selectedProfile
-              ?.selectedEvacueeInContext,
-          householdMemberRegistrantId: undefined,
-          profileReloadFlag: null
-        }
-      };
-      this.computeState.triggerEvent();
-    }
-  }
-
-  private getEvacueeProfile(
-    evacueeProfileId: string
-  ): Promise<RegistrantProfileModel> {
-    const profile$ = this.evacueeProfileService
-      .getProfileFromId(evacueeProfileId)
-      .pipe(
-        tap({
-          next: (profile: RegistrantProfileModel) => {},
-          error: (error) => {
-            this.alertService.clearAlert();
-            this.alertService.setAlert('danger', globalConst.getProfileError);
-          }
-        })
-      );
-    return lastValueFrom(profile$);
-  }
-
-  private hasPostalCode(): boolean {
-    return (
-      this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext
-        ?.primaryAddress.postalCode !== null &&
-      this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext
-        ?.primaryAddress.postalCode !== '' &&
-      this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext
-        ?.primaryAddress.postalCode !== undefined &&
-      this.appBaseService?.appModel?.selectedProfile?.selectedEvacueeInContext
-        ?.primaryAddress?.stateProvince?.code === 'BC'
-    );
   }
 }
