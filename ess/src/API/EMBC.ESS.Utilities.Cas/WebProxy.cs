5f08710786e83797209d8a7dd81e9aebcd6b5faa
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2022-09-21 16:37:53 -0700

    add token endpoint to mock cas

--- ess/src/API/EMBC.ESS.Utilities.Cas/WebProxy.cs
+++ ess/src/API/EMBC.ESS.Utilities.Cas/WebProxy.cs
@@ -23,8 +23,6 @@ namespace EMBC.ESS.Utilities.Cas
 
         private HttpClient httpClient => httpClientFactory.CreateClient("cas");
 
-        private bool skipToken = false;
-
         public WebProxy(IHttpClientFactory httpClientFactory, IOptions<CasConfiguration> config, ICache cache)
         {
             this.httpClientFactory = httpClientFactory;
@@ -36,16 +34,10 @@ namespace EMBC.ESS.Utilities.Cas
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
             this.jsonSerializerOptions.Converters.Add(new CasDateJsonConverter());
-
-            if (httpClient.BaseAddress.ToString().Contains("localhost"))
-            {
-                this.skipToken = true;
-            }
         }
 
         public async Task<string> CreateTokenAsync(CancellationToken ct)
         {
-            if (this.skipToken) return await Task.FromResult("123");
             var request = new ClientCredentialsTokenRequest
             {
                 Address = "oauth/token",
---
+++ mock-cas/src/API/MockCas/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MockCas.Controllers
+{
+    [ApiController]
+    [Route("api/mockcas/oauth/token")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthResponse>> GetToken()
+        {
+            await Task.CompletedTask;
+            return Ok(new AuthResponse
+            {
+                access_token = "123",
+                token_type = "bearer",
+                expires_in = 3000
+            });
+        }
+    }
+
+    public class AuthResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+    }
+}
