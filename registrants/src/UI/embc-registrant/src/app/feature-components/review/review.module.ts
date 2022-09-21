29fef11dcd61771f36557585bb14204fab028f21
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2022-09-21 16:43:23 -0700

    lint fix

--- registrants/src/UI/embc-registrant/src/app/feature-components/review/review.module.ts
+++ registrants/src/UI/embc-registrant/src/app/feature-components/review/review.module.ts
@@ -7,10 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CustomPipeModule } from '../../core/pipe/customPipe.module';
 import { CoreModule } from '../../core/core.module';
-import {
-  RecaptchaFormsModule,
-  RecaptchaModule
-} from 'ng-recaptcha';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
 import { CaptchaV2Component } from 'src/app/core/components/captcha-v2/captcha-v2.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
