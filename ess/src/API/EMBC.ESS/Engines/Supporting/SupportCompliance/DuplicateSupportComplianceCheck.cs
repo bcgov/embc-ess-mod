1d041e25c50e6ed13628a1d5af2836b73141ef5f
KyleKayfish <39177955+KyleKayfish@users.noreply.github.com>
2023-08-30 12:26:55 -0700

    Revert "fix: duplicate check to consider contained date range and minor perfomance improvements"

--- ess/src/API/EMBC.ESS/Engines/Supporting/SupportCompliance/DuplicateSupportComplianceCheck.cs
+++ ess/src/API/EMBC.ESS/Engines/Supporting/SupportCompliance/DuplicateSupportComplianceCheck.cs
@@ -22,42 +22,40 @@ namespace EMBC.ESS.Engines.Supporting.SupportCompliance
         {
             if (support.Id == null) throw new ArgumentNullException(nameof(support.Id));
 
-            var ctx = essContextFactory.CreateReadOnly();
+            var ctx = essContextFactory.Create();
 
             var checkedSupport = (await ((DataServiceQuery<era_evacueesupport>)ctx.era_evacueesupports.Where(s => s.era_name == support.Id)).GetAllPagesAsync()).SingleOrDefault();
             if (checkedSupport == null) throw new ArgumentException($"Support {support.Id} not found", nameof(support));
 
-            ctx.AttachTo(nameof(ctx.era_evacueesupports), checkedSupport);
             await ctx.LoadPropertyAsync(checkedSupport, nameof(era_evacueesupport.era_era_householdmember_era_evacueesupport));
 
             var from = checkedSupport.era_validfrom.Value;
             var to = checkedSupport.era_validto.Value;
             var type = checkedSupport.era_supporttype.Value;
-            var householdMembers = checkedSupport.era_era_householdmember_era_evacueesupport.ToList();
+            var householdMembers = checkedSupport.era_era_householdmember_era_evacueesupport.ToArray();
 
             var similarSupports = (await ((DataServiceQuery<era_evacueesupport>)ctx.era_evacueesupports.Where(s =>
                 s.era_name != checkedSupport.era_name && s.era_supporttype == type &&
-                s.statuscode != (int)Resources.Supports.SupportStatus.Cancelled && s.statuscode != (int)Resources.Supports.SupportStatus.Void &&
-                ((s.era_validfrom >= from && s.era_validfrom <= to) || (s.era_validto >= from && s.era_validto <= to) || (s.era_validfrom < from && s.era_validto > to))))
-                .GetAllPagesAsync()).ToList();
+                s.statuscode != (int)SupportStatus.Cancelled && s.statuscode != (int)SupportStatus.Void &&
+                ((s.era_validfrom >= from && s.era_validfrom <= to) ||
+                (s.era_validto >= from && s.era_validto <= to))))
+                .GetAllPagesAsync()).ToArray();
 
-            similarSupports.AsParallel().ForAll(s =>
+            foreach (var similarSupport in similarSupports)
             {
-                ctx.AttachTo(nameof(ctx.era_evacueesupports), s);
-                ctx.LoadProperty(s, nameof(era_evacueesupport.era_era_householdmember_era_evacueesupport));
-                ctx.Detach(s);
-            });
+                await ctx.LoadPropertyAsync(similarSupport, nameof(era_evacueesupport.era_era_householdmember_era_evacueesupport));
+            }
 
             Func<era_householdmember, era_householdmember, bool> householdMemberMatcher = (m1, m2) =>
-                m1.era_firstname.Equals(m2.era_firstname, StringComparison.InvariantCultureIgnoreCase) &&
-                m1.era_lastname.Equals(m2.era_lastname, StringComparison.InvariantCultureIgnoreCase) &&
-                m1.era_dateofbirth.Value.Equals(m2.era_dateofbirth);
+                m1.era_firstname.Equals(m2.era_firstname, StringComparison.OrdinalIgnoreCase) &&
+                m1.era_lastname.Equals(m2.era_lastname, StringComparison.OrdinalIgnoreCase) &&
+                m1.era_dateofbirth.Equals(m2.era_dateofbirth);
 
             var duplicateSupports = similarSupports
-                .Where(s =>
-                    s.era_era_householdmember_era_evacueesupport.Any(m => householdMembers.Exists(cm => householdMemberMatcher(cm, m))));
+                .Where(s => s.era_era_householdmember_era_evacueesupport.Any(m => householdMembers.Any(cm => householdMemberMatcher(cm, m))))
+                .ToArray();
 
-            var duplicates = duplicateSupports.Select(s => new DuplicateSupportFlag { DuplicatedSupportId = s.era_name }).ToList();
+            var duplicates = duplicateSupports.Select(s => new DuplicateSupportFlag { DuplicatedSupportId = s.era_name }).ToArray();
 
             ctx.DetachAll();
 
