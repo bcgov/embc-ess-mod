d63e29281818e5b83fce37d1e2e18a02f08e7c9e
W. Ross White <ross.white@quartech.com>
2024-03-28 16:42:46 -0700

    accidentally added them back

--- registrants/src/UI/embc-registrant/src/app/core/api/models/needs-assessment.ts
+++ registrants/src/UI/embc-registrant/src/app/core/api/models/needs-assessment.ts
@@ -16,9 +16,6 @@ export interface NeedsAssessment {
   shelterOption?: null | string;
   canEvacueeProvideTransportation?: null | boolean;
   doesEvacueeNotRequireAssistance?: null | boolean;
-  hasPetsFood?: null | boolean;
-  haveMedication?: boolean;
-  haveSpecialDiet?: boolean;
   householdMembers?: Array<HouseholdMember>;
   id?: null | string;
   insurance: InsuranceOption;
