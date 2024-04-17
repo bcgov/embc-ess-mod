69d42528a56b64e0eee30eab80801e364ac55ac1
Hemanth Kona <Hemanth.Kona@quartech.com>
2024-04-17 12:02:20 -0400

    Update @angular to 15, @angular-eslint to 15

--- registrants/src/UI/embc-registrant/src/app/app-routing.module.ts
+++ registrants/src/UI/embc-registrant/src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
