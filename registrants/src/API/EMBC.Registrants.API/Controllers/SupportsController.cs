2a959f81b814d13f951d92a17f53ab34156e969c
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-04-17 14:59:31 -0700

    fix(self-serve): authorization on controller actions

--- registrants/src/API/EMBC.Registrants.API/Controllers/SupportsController.cs
+++ registrants/src/API/EMBC.Registrants.API/Controllers/SupportsController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EMBC.Registrants.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class SupportsController : ControllerBase
 {
     [HttpGet("{evacuationFileId}/supports/draft")]
