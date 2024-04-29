eb07e6c30b8a74e5c516f13e58420c5e46555187
ytqsl <yossi.tamari@quartech.com>
2024-04-29 07:25:50 -0700

    WIP: generate self serve supports

--- ess/src/API/EMBC.ESS/Engines/Supporting/SupportGeneration/SelfServe/SelfServeSupportGenerator.cs
+++ ess/src/API/EMBC.ESS/Engines/Supporting/SupportGeneration/SelfServe/SelfServeSupportGenerator.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EMBC.ESS.Shared.Contracts.Events;
 using EMBC.ESS.Shared.Contracts.Events.SelfServe;
 
 namespace EMBC.ESS.Engines.Supporting.SupportGeneration.SelfServe
@@ -31,17 +34,49 @@ namespace EMBC.ESS.Engines.Supporting.SupportGeneration.SelfServe
         private async Task<GenerateResponse> Handle(GenerateSelfServeSupports req, CancellationToken ct)
         {
             var householdMembers = req.HouseholdMembersIds.ToArray();
-            DateOnly[] days = [DateOnly.FromDateTime(req.From.Date), DateOnly.FromDateTime(DateTime.Now.AddDays(1)), DateOnly.FromDateTime(DateTime.Now.AddDays(2))];
-            var supports = new SelfServeSupport[]
+            var days = CalculateSupportDays(req.From, req.To);
+            var supports = new List<SelfServeSupport>();
+            foreach (var need in req.Needs)
             {
-                new SelfServeClothingSupport { IncludedHouseholdMembers = householdMembers, TotalAmount = 100d },
-                new SelfServeFoodGroceriesSupport { Nights = days.Select(d=>new SupportDay(d,householdMembers)), TotalAmount = 100d },
-                new SelfServeFoodRestaurantSupport { IncludedHouseholdMembers = householdMembers, TotalAmount = 100d },
-                new SelfServeIncidentalsSupport { IncludedHouseholdMembers = householdMembers, TotalAmount = 100d },
-                new SelfServeShelterAllowanceSupport { Nights = days.Select(d=>new SupportDay(d,householdMembers)), TotalAmount = 100d },
-            };
+                supports.AddRange(CreateSupportForNeed(need, days, householdMembers));
+            }
 
             return await Task.FromResult(new GenerateSelfServeSupportsResponse(supports));
         }
+
+        private static IEnumerable<DateOnly> CalculateSupportDays(DateTimeOffset from, DateTimeOffset to)
+        {
+            while (from < to)
+            {
+                yield return DateOnly.FromDateTime(from.Date);
+                from = from.AddDays(1);
+            }
+        }
+
+        private static IEnumerable<SelfServeSupport> CreateSupportForNeed(IdentifiedNeed need, IEnumerable<DateOnly> days, IEnumerable<string> householdMembers) =>
+         need switch
+         {
+             IdentifiedNeed.Food =>
+                new SelfServeSupport[]
+                {
+                    new SelfServeFoodGroceriesSupport { Nights = days.Select(d=> new SupportDay(d,householdMembers)), TotalAmount = 0},
+                    new SelfServeFoodRestaurantSupport
+                    {
+                        IncludedHouseholdMembers = householdMembers,
+                        Meals = days.Select(d=>new SupportDayMeals(d, true, true, true)),
+                        TotalAmount = 100d,
+                    }
+                },
+
+             IdentifiedNeed.Incidentals => [new SelfServeIncidentalsSupport { IncludedHouseholdMembers = householdMembers, TotalAmount = 100d }],
+             IdentifiedNeed.Clothing => [new SelfServeClothingSupport { IncludedHouseholdMembers = householdMembers, TotalAmount = 100d }],
+             IdentifiedNeed.ShelterAllowance => [new SelfServeShelterAllowanceSupport { Nights = days.Select(d => new SupportDay(d, householdMembers)), TotalAmount = 100d }],
+
+             _ => throw new NotImplementedException($"{need.GetType().Name}")
+         };
+
+         private static void CalculateAmount(SelfServeFoodGroceriesSupport support){
+             support.TotalAmount = 100d;
+         }
     }
 }
