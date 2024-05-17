59229b8357fa6161ca26a7b21da5a659c37b11aa
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-05-17 09:34:44 -0700

    EMBCESSMOD-5374: added support config to task query

--- ess/src/API/EMBC.ESS/Resources/Tasks/Mappings.cs
+++ ess/src/API/EMBC.ESS/Resources/Tasks/Mappings.cs
@@ -1,33 +1,29 @@
 ﻿using System;
+using System.Linq;
 using AutoMapper;
 using EMBC.ESS.Utilities.Dynamics.Microsoft.Dynamics.CRM;
 
-namespace EMBC.ESS.Resources.Tasks
+namespace EMBC.ESS.Resources.Tasks;
+
+public class Mappings : Profile
 {
-    public class Mappings : Profile
+    public Mappings()
     {
-        public Mappings()
-        {
-            CreateMap<era_task, EssTask>()
-                .ForMember(d => d.Id, opts => opts.MapFrom(s => s.era_name))
-                .ForMember(d => d.Description, opts => opts.MapFrom(s => s.era_taskdetails))
-                .ForMember(d => d.Status, opts => opts.MapFrom(s => s.statuscode))
-                .ForMember(d => d.CommunityCode, opts => opts.MapFrom(s => s.era_JurisdictionID.era_jurisdictionid))
-                .ForMember(d => d.StartDate, opts => opts.MapFrom(s => s.era_taskstartdate.HasValue ? s.era_taskstartdate.Value.UtcDateTime : (DateTime?)null))
-                .ForMember(d => d.EndDate, opts => opts.MapFrom(s => s.era_taskenddate.HasValue ? s.era_taskenddate.Value.UtcDateTime : (DateTime?)null))
-                .ForMember(d => d.AutoApprovedEnabled, opts => opts.MapFrom(s => false))
-                .ForMember(d => d.RemoteExtensionsEnabled, opts => opts.MapFrom(s => s.era_remoteextensiontoggle))
-                ;
-        }
+        CreateMap<era_task, EssTask>()
+            .ForMember(d => d.Id, opts => opts.MapFrom(s => s.era_name))
+            .ForMember(d => d.Description, opts => opts.MapFrom(s => s.era_taskdetails))
+            .ForMember(d => d.Status, opts => opts.MapFrom(s => s.statuscode))
+            .ForMember(d => d.CommunityCode, opts => opts.MapFrom(s => s.era_JurisdictionID.era_jurisdictionid))
+            .ForMember(d => d.StartDate, opts => opts.MapFrom(s => s.era_taskstartdate.HasValue ? s.era_taskstartdate.Value.UtcDateTime : (DateTime?)null))
+            .ForMember(d => d.EndDate, opts => opts.MapFrom(s => s.era_taskenddate.HasValue ? s.era_taskenddate.Value.UtcDateTime : (DateTime?)null))
+            .ForMember(d => d.RemoteExtensionsEnabled, opts => opts.MapFrom(s => s.era_remoteextensiontoggle))
+            .ForMember(d => d.SelfServeEnabled, opts => opts.MapFrom(s => s.era_selfservetoggle))
+            .ForMember(d => d.AutoApprovedEnabled, opts => opts.Ignore())
+            .ForMember(d => d.EnabledSupports, opts => opts.MapFrom(s => s.era_era_task_era_selfservesupportlimits_Task.Where(sl => sl.statuscode == 1)))
+            ;
+
+        CreateMap<era_selfservesupportlimits, SupportConfiguration>()
+            .ForMember(d => d.SupportType, opts => opts.MapFrom(s => s.era_supporttypeoption))
+            ;
     }
-
-#pragma warning disable CA1008 // Enums should have zero value
-
-    public enum EssTaskStatusCode
-    {
-        Active = 1,
-        Expired = 2,
-    }
-
-#pragma warning restore CA1008 // Enums should have zero value
 }
