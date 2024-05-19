a075dd798f927bbbf1a911002525021d9176c6bc
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-05-18 19:40:54 -0700

    EMBCESSMOD-5370 - Shelter allowance calculation adjusted per household for responders (#2186)
    * EMBCESSMOD-5370 - Shelter allowance calculation adjusted per household for responders
    
    * EMBCESSMOD-5370: prettier fix
    
    ---------
    
    Co-authored-by: TimA <tim.asadov@quartech.com>

--- responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/support-details/details-type/shelter-allowance/shelter-allowance.component.ts
+++ responders/src/UI/embc-responder/src/app/feature-components/wizard/support-components/support-details/details-type/shelter-allowance/shelter-allowance.component.ts
@@ -36,47 +36,17 @@ export class ShelterAllowanceGroupComponent implements OnInit, OnChanges, AfterV
       this.referralForm.get('noOfNights').patchValue(this.noOfDays);
       this.updateTotalAmount();
     }
-    if (changes.selectedHouseholdMembers) {
-      this.updateTotalAmount();
-    }
 
     this.referralForm.get('noOfNights').valueChanges.subscribe(() => {
       this.updateTotalAmount();
     });
   }
   updateTotalAmount() {
-    this.members = this.supportDetailsForm.get('members').value;
     this.nofNight = this.referralForm.get('noOfNights').value;
+    this.totalAmount = this.nofNight * globalConst.shelterAllowanceRate.rate;
 
-    this.totalAmount = 0;
-    if (this.members != null && this.members.length === 1) {
-      this.totalAmount = this.nofNight * globalConst.shelterAllowanceRate.rate;
-    } else if (this.members != null && this.members.length > 1) {
-      const count = this.members.reduce(
-        (acc, member) => {
-          if (member.isMinor) {
-            acc.minorCount++;
-          } else {
-            acc.adultCount++;
-          }
-          return acc;
-        },
-        { minorCount: 0, adultCount: 0 }
-      );
+    if (this.totalAmount < 0) this.totalAmount = 0;
 
-      if (count.adultCount === 0) {
-        this.totalAmount =
-          this.nofNight *
-          (globalConst.shelterAllowanceRate.rate + (count.minorCount - 1) * globalConst.shelterAllowanceRate.child);
-      } else {
-        this.totalAmount =
-          this.nofNight *
-          (globalConst.shelterAllowanceRate.rate +
-            count.minorCount * globalConst.shelterAllowanceRate.child +
-            (count.adultCount - 1) * globalConst.shelterAllowanceRate.adult);
-      }
-      if (this.totalAmount < 0) this.totalAmount = 0;
-    }
     this.referralForm.get('totalAmount').patchValue(this.totalAmount);
 
     this.checkOverlimit(this.totalAmount);
