f40e0965e28ca6e5e76243c187c10f8989b4e992
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2024-06-04 15:49:56 -0700

    EMBCESSMOD-5523: check compliance duplicate support query improvements

--- ess/src/API/EMBC.ESS/Engines/Supporting/SupportCompliance/DuplicateSupportComplianceCheck.cs
+++ ess/src/API/EMBC.ESS/Engines/Supporting/SupportCompliance/DuplicateSupportComplianceCheck.cs
@@ -7,12 +7,11 @@ using EMBC.ESS.Shared.Contracts.Events;
 using EMBC.ESS.Utilities.Dynamics;
 using EMBC.ESS.Utilities.Dynamics.Microsoft.Dynamics.CRM;
 using EMBC.Utilities.Extensions;
-using Microsoft.Extensions.Logging;
 using Microsoft.OData.Client;
 
 namespace EMBC.ESS.Engines.Supporting.SupportCompliance
 {
-    internal class DuplicateSupportComplianceCheck(IEssContextFactory essContextFactory, ILogger<DuplicateSupportComplianceCheck> logger) : ISupportComplianceCheck
+    internal class DuplicateSupportComplianceCheck(IEssContextFactory essContextFactory) : ISupportComplianceCheck
     {
         public async Task<IEnumerable<SupportFlag>> CheckCompliance(Support support, CancellationToken ct)
         {
@@ -34,23 +33,19 @@ namespace EMBC.ESS.Engines.Supporting.SupportCompliance
 
             var similarSupportTypes = SimilarSupportTypes(Enum.Parse<SupportType>(type.ToString())).Cast<int>().ToArray();
 
-            var similarSupportsQuery = ctx.era_evacueesupports
-                .WhereIn(s => s.era_supporttype.Value, similarSupportTypes)
-                .WhereNotIn(s => s.statuscode.Value, [(int)Resources.Supports.SupportStatus.Cancelled, (int)Resources.Supports.SupportStatus.Void])
-                .Where(s => s.era_name != support.Id)
-                .Where(s => (s.era_validfrom >= from && s.era_validfrom <= to) || (s.era_validto >= from && s.era_validto <= to) || (s.era_validfrom < from && s.era_validto > to));
-
-            var similarSupports = (await similarSupportsQuery.GetAllPagesAsync(ct)).DistinctBy(s => s.era_name).ToList();
-
-            similarSupports.AsParallel().ForAll(s =>
+            var duplicateSupports = new List<era_evacueesupport>();
+            foreach (var hm in householdMembers)
             {
-                if (s.era_name == checkedSupport.era_name) logger.LogDebug("{0} {1}", checkedSupport.era_name, s.era_name);
-                ctx.AttachTo(nameof(ctx.era_evacueesupports), s);
-                ctx.LoadProperty(s, nameof(era_evacueesupport.era_era_householdmember_era_evacueesupport));
-                ctx.Detach(s);
-            });
+                var duplicateSupportsQuery = ctx.era_evacueesupports
+                    .WhereIn(s => s.era_supporttype.Value, similarSupportTypes)
+                    .WhereNotIn(s => s.statuscode.Value, [(int)Resources.Supports.SupportStatus.Cancelled, (int)Resources.Supports.SupportStatus.Void])
+                    .Where(s => s.era_name != support.Id)
+                    .Where(s =>
+                            s.era_era_householdmember_era_evacueesupport.Any(h => h.era_dateofbirth == hm.era_dateofbirth && h.era_firstname == hm.era_firstname && h.era_lastname == hm.era_lastname) &&
+                            ((s.era_validfrom >= from && s.era_validfrom <= to) || (s.era_validto >= from && s.era_validto <= to) || (s.era_validfrom < from && s.era_validto > to)));
 
-            var duplicateSupports = similarSupports.Where(s => s.era_era_householdmember_era_evacueesupport.Any(m => householdMembers.Exists(cm => IsSameHouseholdMember(cm, m))));
+                duplicateSupports = duplicateSupports.Concat(await duplicateSupportsQuery.GetAllPagesAsync(ct)).DistinctBy(s => s.era_name).ToList();
+            }
 
             var duplicates = duplicateSupports.Select(s => new DuplicateSupportFlag { DuplicatedSupportId = s.era_name }).ToList();
 
@@ -58,11 +53,6 @@ namespace EMBC.ESS.Engines.Supporting.SupportCompliance
 
             return duplicates;
         }
-
-        private static bool IsSameHouseholdMember(era_householdmember m1, era_householdmember m2) =>
-            m1.era_firstname.Equals(m2.era_firstname, StringComparison.InvariantCultureIgnoreCase) &&
-            m1.era_lastname.Equals(m2.era_lastname, StringComparison.InvariantCultureIgnoreCase) &&
-            m1.era_dateofbirth.Value.Equals(m2.era_dateofbirth);
 
         private static SupportType[] SimilarSupportTypes(SupportType type) =>
             type switch
